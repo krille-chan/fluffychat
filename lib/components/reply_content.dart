bc18801ae33312bd330303f0bff13dea6fab4c1e
Christian Pauly <christianpauly@Christians-Air.fritz.box>
2020-02-15 10:31:51 +0100

    Fix reply design

--- lib/components/reply_content.dart
+++ lib/components/reply_content.dart
@@ -36,7 +36,7 @@ class ReplyContent extends StatelessWidget {
               ),
               Text(
                 replyEvent?.getLocalizedBody(context,
-                        withSenderNamePrefix: false) ??
+                        withSenderNamePrefix: false, hideQuotes: true) ??
                     "",
                 overflow: TextOverflow.ellipsis,
                 maxLines: 1,
