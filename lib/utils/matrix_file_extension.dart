a0e841c9c6e9e7dbc6e5f3c7c6ad8c4fe49bf13b
Christian Pauly <christian-pauly@posteo.de>
2020-04-10 18:25:47 +0200

    Open file in web

--- lib/utils/matrix_file_extension.dart
+++ lib/utils/matrix_file_extension.dart
@@ -1,15 +1,32 @@
 import 'dart:io';
 
 import 'package:famedlysdk/famedlysdk.dart';
+import 'package:flutter/foundation.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:universal_html/prefer_universal/html.dart' as html;
+import 'package:mime_type/mime_type.dart';
 
 extension MatrixFileExtension on MatrixFile {
   void open() async {
     Directory tempDir = await getTemporaryDirectory();
     final file = File(tempDir.path + "/" + path.split("/").last);
     file.writeAsBytesSync(bytes);
-    await OpenFile.open(file.path);
+    if (kIsWeb) {
+      final fileName = path.split('/').last;
+      final mimeType = mime(fileName);
+      var element = html.document.createElement('a');
+      element.setAttribute(
+          'href', html.Url.createObjectUrlFromBlob(html.Blob([bytes])));
+      element.setAttribute('target', "new");
+      element.setAttribute('type', mimeType);
+      element.style.display = 'none';
+      html.document.body.append(element);
+      element.click();
+      element.remove();
+    } else {
+      await OpenFile.open(file.path);
+    }
     return;
   }
 }
