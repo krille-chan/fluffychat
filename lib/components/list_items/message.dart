4988e4eb63c0eccee4fc73e178fb4f55a37952a5
Christian Pauly <christian-pauly@posteo.de>
2020-05-09 13:40:55 +0000

    Fix #55

--- lib/components/list_items/message.dart
+++ lib/components/list_items/message.dart
@@ -41,8 +41,8 @@ class Message extends StatelessWidget {
     final bool ownMessage = event.senderId == client.userID;
     Alignment alignment = ownMessage ? Alignment.topRight : Alignment.topLeft;
     Color color = Theme.of(context).secondaryHeaderColor;
-    final bool sameSender = nextEvent != null &&
-            [EventTypes.Message, EventTypes.Sticker].contains(nextEvent.type)
+    final bool isImage = [EventTypes.Message, EventTypes.Sticker].contains(nextEvent.type);
+    final bool sameSender = nextEvent != null && isImage
         ? nextEvent.sender.id == event.sender.id
         : false;
     BubbleNip nip = sameSender
@@ -56,7 +56,7 @@ class Message extends StatelessWidget {
     MainAxisAlignment rowMainAxisAlignment =
         ownMessage ? MainAxisAlignment.end : MainAxisAlignment.start;
 
-    if (event.showThumbnail) {
+    if (isImage && event.showThumbnail) {
       color = Theme.of(context).scaffoldBackgroundColor.withOpacity(0.66);
       textColor = Theme.of(context).textTheme.bodyText2.color;
     } else if (ownMessage) {
