80f6e218974abb7db1e121257a9b3489b7503685
Sorunome <mail@sorunome.de>
2020-09-23 18:26:04 +0200

    fix: determine 12h/24h time based on settings, not locale

--- lib/utils/date_time_extension.dart
+++ lib/utils/date_time_extension.dart
@@ -34,8 +34,11 @@ extension DateTimeExtension on DateTime {
   /// Returns a simple time String.
   /// TODO: Add localization
   String localizedTimeOfDay(BuildContext context) {
-    return L10n.of(context).timeOfDay(_z(hour % 12 == 0 ? 12 : hour % 12),
-        _z(hour), _z(minute), hour > 11 ? 'pm' : 'am');
+    if (MediaQuery.of(context).alwaysUse24HourFormat) {
+      return '${_z(hour)}:${_z(minute)}';
+    } else {
+      return '${_z(hour % 12 == 0 ? 12 : hour % 12)}:${_z(minute)} ${hour > 11 ? "pm" : "am"}';
+    }
   }
 
   /// Returns [localizedTimeOfDay()] if the ChatTime is today, the name of the week
