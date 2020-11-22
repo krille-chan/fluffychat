a5c7e4baaf4b2c88c3803255c601f7f52520557e
Christian Pauly <christian-pauly@posteo.de>
2020-11-22 10:04:47 +0000

    chore: Implement playstore CD

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-cd android && echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks && cd ..
-cd android && echo "storePassword=${FDROID_KEY_PASS}" >> key.properties && cd ..
-cd android && echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties && cd ..
-cd android && echo "keyAlias=key" >> key.properties && cd ..
-cd android && echo "storeFile=../key.jks" >> key.properties && cd ..
-cd android/app && echo $GOOGLE_SERVICES >> google-services.json && cd ../..
+cd android
+echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
+echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
+echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
+echo "keyAlias=key" >> key.properties
+echo "storeFile=../key.jks" >> key.properties
+echo $PLAYSTORE_DEPLOY_KEY >> keys.json
+bundle install
+bundle update fastlane
+bundle exec fastlane set_build_code_internal
+cd app
+echo $GOOGLE_SERVICES >> google-services.json/..
+cd ../..
--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -5,3 +5,6 @@ flutter pub get
 flutter build appbundle --target-platform android-arm,android-arm64,android-x64
 mkdir -p build/android
 cp build/app/outputs/bundle/release/app-release.aab build/android/
+cd android
+bundle exec fastlane deploy_internal_test
+cd ..
