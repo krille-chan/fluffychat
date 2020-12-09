d8f3712c5a867ee16769c054af18c8646b5df35f
Christian Pauly <christian-pauly@posteo.de>
2020-12-09 06:18:34 +0000

    fix: CI

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -6,6 +6,7 @@ echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
 echo $PLAYSTORE_DEPLOY_KEY >> keys.json
+ls | grep key
 bundle install
 bundle update fastlane
 bundle exec fastlane set_build_code_internal
