1897172d1e0cdd7f49391bd82cd7b9b6b7341c24
Christian Pauly <christian-pauly@posteo.de>
2020-12-09 20:03:51 +0000

    Update scripts/build-android-apk.sh, scripts/build-android-debug.sh, scripts/prepare-android-release.sh files

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -ve
+#!/usr/bin/env bash -v
 flutter channel stable
 flutter upgrade
 flutter build apk --debug -v
--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -ve
+#!/usr/bin/env bash -v
 cd android
 echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
