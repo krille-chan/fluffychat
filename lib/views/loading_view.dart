566f893f8f9a3cfe8025515bfcb948ff3f6ef4ca
Christian Pauly <christian-pauly@posteo.de>
2021-01-17 08:05:34 +0100

    fix: Change size

--- lib/views/loading_view.dart
+++ lib/views/loading_view.dart
@@ -1,8 +1,14 @@
+import 'package:adaptive_page_layout/adaptive_page_layout.dart';
+import 'package:fluffychat/components/matrix.dart';
 import 'package:flutter/material.dart';
 
 class LoadingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (Matrix.of(context).loginState != null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) =>
+          AdaptivePageLayout.of(context).pushNamedAndRemoveAllOthers('/'));
+    }
     return Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
