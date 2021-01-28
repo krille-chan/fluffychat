80505a321a1c4f2b8805879b739c2ffed545e277
Sorunome <mail@sorunome.de>
2021-01-28 18:25:22 +0100

    a CI a day keeps the doctor away

--- scripts/build-android-apk.sh
+++ scripts/build-android-apk.sh
@@ -2,8 +2,6 @@
 flutter channel stable
 flutter upgrade
 flutter pub get
-flutter build apk --debug
-flutter build apk --profile
 flutter build apk --release
 mkdir -p build/android
 cp build/app/outputs/apk/release/app-release.apk build/android/
