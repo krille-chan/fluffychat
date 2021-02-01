84aae27c7c466e951230bf1c38109df0d2cd9a11
Krille Fear <christian-pauly@posteo.de>
2021-02-01 21:18:50 +0000

    Update scripts/build-linux.sh

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -ve
-flutter channel dev
+flutter channel master
 flutter upgrade
 flutter config --enable-linux-desktop
-echo "dependency_overrides:\n  intl: 0.17.0-nullsafety.2" >> pubspec.yaml
 flutter clean
 flutter pub get
 flutter build linux --release -v
