0d1a80adf0c6dfbd35911818b7a9ee9265a90f97
Christian Pauly <christian-pauly@posteo.de>
2021-03-06 09:33:06 +0100

    CI: Fix snapcraft job

--- scripts/build-snap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ve
-'curl -L $(curl -H "X-Ubuntu-Series: 16" "https://api.snapcraft.io/api/v1/snaps/details/flutter?channel=latest/stable" | jq ".download_url" -r) --output flutter.snap'
-sudo mkdir -p /snap/flutter
-sudo unsquashfs -d /snap/flutter/current flutter.snap
-rm -f flutter.snap
-sudo ln -sf /snap/flutter/current/flutter.sh /snap/bin/flutter
-sudo ln -sf /snap/flutter/current/env.sh /snap/bin/env.sh
-snapcraft
--- scripts/publish-snap-stable.sh
+++ scripts/publish-snap-stable.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ve
-/bin/bash ./scripts/build-snap.sh
+snapcraft
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > snapcraft.login
 snapcraft login --with snapcraft.login
 snapcraft push --release=stable *.snap
