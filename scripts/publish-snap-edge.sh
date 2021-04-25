616734c0170f3b123708303dee02a830c2e22458
Christian Pauly <christian-pauly@posteo.de>
2021-04-25 08:48:32 +0200

    CI: Fix Snap build

--- scripts/publish-snap-edge.sh
+++ scripts/publish-snap-edge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ve
-snapcraft
+snapcraft remote-build
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > snapcraft.login
 snapcraft login --with snapcraft.login
 snapcraft push --release=edge *.snap
