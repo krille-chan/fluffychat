cf62578d88d92d66955ec6f45ef9c63082d1866e
Krille Fear <christian-pauly@posteo.de>
2021-04-25 11:09:22 +0000

    Update scripts/publish-snap-edge.sh, scripts/publish-snap-stable.sh files

--- scripts/publish-snap-edge.sh
+++ scripts/publish-snap-edge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > snapcraft.login
 snapcraft login --with snapcraft.login
-snapcraft remote-build --launchpad-accept-public-upload
+snapcraft
 snapcraft push --release=edge *.snap
 snapcraft logout
--- scripts/publish-snap-stable.sh
+++ scripts/publish-snap-stable.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > snapcraft.login
 snapcraft login --with snapcraft.login
-snapcraft remote-build --launchpad-accept-public-upload
+snapcraft
 snapcraft push --release=stable *.snap
 snapcraft logout
