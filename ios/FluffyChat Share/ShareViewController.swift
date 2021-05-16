ae8c5caf9e6ca4d02e8fd73857be9c271a134ca7
Christian Pauly <christian-pauly@posteo.de>
2021-05-16 16:21:02 +0200

    chore: Format iOS stuff

--- ios/FluffyChat Share/ShareViewController.swift
+++ ios/FluffyChat Share/ShareViewController.swift
@@ -5,7 +5,7 @@ import Photos
 
 class ShareViewController: SLComposeServiceViewController {
     // TODO: IMPORTANT: This should be your host app bundle identifier
-    let hostAppBundleIdentifier = "com.kasem.sharing"
+    let hostAppBundleIdentifier = "im.fluffychat.app"
     let sharedKey = "ShareKey"
     var sharedMedia: [SharedMediaFile] = []
     var sharedText: [String] = []
