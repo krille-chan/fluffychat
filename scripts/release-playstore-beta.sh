b794e4aa7b0731b62c8a07b78d2a9cf6790e4ad9
Krille Fear <christian-pauly@posteo.de>
2021-05-30 11:55:05 +0000

    Update .gitlab-ci.yml, scripts/release-playstore-beta.sh files

--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -4,5 +4,7 @@ flutter build appbundle --target-platform android-arm,android-arm64,android-x64
 mkdir -p build/android
 cp build/app/outputs/bundle/release/app-release.aab build/android/
 cd android
+bundle install
+bundle update fastlane
 bundle exec fastlane deploy_internal_test
 cd ..
