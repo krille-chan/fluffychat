51a0fd130d0d7db3b274595cf2f25faf3907f2d8
Christian Pauly <christian-pauly@posteo.de>
2021-06-20 12:44:23 +0200

    change: Linux default window size

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -48,7 +48,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "fluffychat");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 800, 600);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
