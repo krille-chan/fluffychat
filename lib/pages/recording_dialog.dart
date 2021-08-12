cdfb97ab5850cf45a0d35411d243b3586890f54e
Christian Pauly <christian-pauly@posteo.de>
2021-08-12 09:48:10 +0200

    feat: Cupertino style record dialog

--- lib/pages/recording_dialog.dart
+++ lib/pages/recording_dialog.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/sentry_controller.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:path_provider/path_provider.dart';
@@ -81,38 +83,65 @@ class _RecordingDialogState extends State<RecordingDialog> {
             (maxDecibalWidth / 4).toDouble();
     final time =
         '${_duration.inMinutes.toString().padLeft(2, '0')}:${(_duration.inSeconds % 60).toString().padLeft(2, '0')}';
-    print('Decibal width: $decibalWidth');
-    return AlertDialog(
-      content: error
-          ? Text(L10n.of(context).oopsSomethingWentWrong)
-          : Row(
-              children: <Widget>[
-                Container(
-                  width: maxDecibalWidth,
-                  height: maxDecibalWidth,
-                  alignment: Alignment.center,
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 100),
-                    width: decibalWidth,
-                    height: decibalWidth,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(decibalWidth),
-                    ),
+    final content = error
+        ? Text(L10n.of(context).oopsSomethingWentWrong)
+        : Row(
+            children: <Widget>[
+              Container(
+                width: maxDecibalWidth,
+                height: maxDecibalWidth,
+                alignment: Alignment.center,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 100),
+                  width: decibalWidth,
+                  height: decibalWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(decibalWidth),
                   ),
                 ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: Text(
-                    '${L10n.of(context).recording}: $time',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  '${L10n.of(context).recording}: $time',
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-              ],
+              ),
+            ],
+          );
+    if (PlatformInfos.isCupertinoStyle) {
+      return CupertinoAlertDialog(
+        content: content,
+        actions: [
+          CupertinoDialogAction(
+            onPressed: () => Navigator.of(context, rootNavigator: false).pop(),
+            child: Text(
+              L10n.of(context).cancel.toUpperCase(),
+              style: TextStyle(
+                color:
+                    Theme.of(context).textTheme.bodyText2.color.withAlpha(150),
+              ),
             ),
-      actions: <Widget>[
+          ),
+          if (error != true)
+            CupertinoDialogAction(
+              onPressed: () async {
+                _recorderSubscription?.cancel();
+                await _audioRecorder.stop();
+                Navigator.of(context, rootNavigator: false)
+                    .pop<String>(_recordedPath);
+              },
+              child: Text(L10n.of(context).send.toUpperCase()),
+            ),
+        ],
+      );
+    }
+    return AlertDialog(
+      content: content,
+      actions: [
         TextButton(
           onPressed: () => Navigator.of(context, rootNavigator: false).pop(),
           child: Text(
