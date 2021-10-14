c2f23c491ece6ea701133d987f1a5b2d264b891d
Krille Fear <christian-pauly@posteo.de>
2021-10-14 17:26:20 +0200

    remove: Android filepicker

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ve
+git apply ./scripts/enable-android-google-services.patch
 flutter format lib/ test/ --set-exit-if-changed
 flutter analyze
-git apply ./scripts/enable-android-google-services.patch --check
