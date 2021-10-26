7e5a460dde989c13ab9696001174057441afe71f
Krille Fear <christian-pauly@posteo.de>
2021-10-26 19:03:17 +0200

    fix: Analyze format and imports before patching

--- scripts/code_analyze.sh
+++ scripts/code_analyze.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
-git apply ./scripts/enable-android-google-services.patch
-flutter format lib/ test/ --set-exit-if-changed
 flutter pub run import_sorter:main --no-comments --exit-if-changed
+flutter format lib/ test/ --set-exit-if-changed
+git apply ./scripts/enable-android-google-services.patch
 flutter analyze
 flutter pub run dart_code_metrics:metrics lib -r gitlab > code-quality-report.json || true
