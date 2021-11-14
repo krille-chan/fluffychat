7b14f0160f4d59387486d85cd6b6527b7c4e60e4
Krille Fear <christian-pauly@posteo.de>
2021-11-14 10:54:39 +0100

    fix: Snap disable remote build

--- scripts/publish-snap-edge.sh
+++ scripts/publish-snap-edge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ve
 echo $SNAPCRAFT_LOGIN_FILE | snapcraft login --with -
-snapcraft remote-build --launchpad-accept-public-upload --status
+snapcraft
 snapcraft upload --release=edge *.snap
 snapcraft logout
