042fd8072b6bb5b3f361d06b37ecb616b5a9deac
Krille Fear <christian-pauly@posteo.de>
2021-11-19 11:02:05 +0100

    fix: Stickers always in square

--- lib/pages/chat/events/sticker.dart
+++ lib/pages/chat/events/sticker.dart
@@ -21,24 +21,11 @@ class _StickerState extends State<Sticker> {
 
   @override
   Widget build(BuildContext context) {
-    // stickers should default to a ratio of 1:1
-    var ratio = 1.0;
-    // if a width and a height is specified for stickers, use those!
-    if (widget.event.infoMap['w'] is int && widget.event.infoMap['h'] is int) {
-      ratio = widget.event.infoMap['w'] / widget.event.infoMap['h'];
-      // make sure the ratio is within 0.9 - 2.0
-      if (ratio > 2.0) {
-        ratio = 2.0;
-      }
-      if (ratio < 0.9) {
-        ratio = 0.9;
-      }
-    }
     return ImageBubble(
       widget.event,
       width: 400,
-      height: 400 / ratio,
-      fit: ratio <= 1.0 ? BoxFit.contain : BoxFit.cover,
+      height: 400,
+      fit: BoxFit.contain,
       onTap: () {
         setState(() => animated = true);
         showOkAlertDialog(
