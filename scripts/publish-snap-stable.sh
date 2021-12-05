e0ba16cdfea30edcb9fff7ccc8fd79cf054599f8
Krille Fear <christian-pauly@posteo.de>
2021-12-05 14:21:47 +0100

    follow: Fix CI release type check

--- scripts/publish-snap-stable.sh
+++ scripts/publish-snap-stable.sh
@@ -2,7 +2,7 @@
 RELEASE_TYPE=$(echo $CI_COMMIT_TAG | grep -oE "[a-z]+")
 echo $SNAPCRAFT_LOGIN_FILE | snapcraft login --with -
 snapcraft
-if [RELEASE_TYPE = "rc"]; then
+if [ "$RELEASE_TYPE" = "rc" ]; then
     snapcraft upload --release=candidate *.snap
 else
     snapcraft upload --release=stable *.snap
--- scripts/release-playstore.sh
+++ scripts/release-playstore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ve
 RELEASE_TYPE=$(echo $CI_COMMIT_TAG | grep -oE "[a-z]+")
 cd android
-if [RELEASE_TYPE = "rc"]; then
+if [ "$RELEASE_TYPE" = "rc" ]; then
     bundle exec fastlane deploy_candidate
 else
     bundle exec fastlane deploy_release
