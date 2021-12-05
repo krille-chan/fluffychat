148c4aa1dd1f86f97c32ce6c87fe1eaeef808f44
Krille Fear <christian-pauly@posteo.de>
2021-12-05 09:13:23 +0100

    CI: Add candidate release pipeline

--- scripts/publish-snap-stable.sh
+++ scripts/publish-snap-stable.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -ve
 echo $SNAPCRAFT_LOGIN_FILE | snapcraft login --with -
 snapcraft
-snapcraft upload --release=stable *.snap
+if [RELEASE_TYPE = "rc"]; then
+    snapcraft upload --release=candidate *.snap
+else
+    snapcraft upload --release=stable *.snap
+fi
 snapcraft logout
--- scripts/release-playstore.sh
+++ scripts/release-playstore.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -ve
 cd android
-bundle exec fastlane deploy_release
+if [RELEASE_TYPE = "rc"]; then
+    bundle exec fastlane deploy_candidate
+else
+    bundle exec fastlane deploy_release
+fi
 cd ..
