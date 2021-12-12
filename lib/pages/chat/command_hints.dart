3c14cbe017915428d323d41f5698181b2442593b
Steef Hegeman <mail@steefhegeman.com>
2021-12-12 10:56:20 +0000

    refactor: /command hints add tooltips, test for missing hints, script to generate glue code, hints for dm, create, clearcache, discardsession

---
+++ lib/pages/chat/command_hints.dart
@@ -0,0 +1,46 @@
+// This file is auto-generated using scripts/generate_command_hints_glue.sh.
+
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+String commandHint(L10n l10n, String command) {
+  switch (command) {
+    case "ban":
+      return l10n.commandHint_ban;
+    case "clearcache":
+      return l10n.commandHint_clearcache;
+    case "create":
+      return l10n.commandHint_create;
+    case "discardsession":
+      return l10n.commandHint_discardsession;
+    case "dm":
+      return l10n.commandHint_dm;
+    case "html":
+      return l10n.commandHint_html;
+    case "invite":
+      return l10n.commandHint_invite;
+    case "join":
+      return l10n.commandHint_join;
+    case "kick":
+      return l10n.commandHint_kick;
+    case "leave":
+      return l10n.commandHint_leave;
+    case "me":
+      return l10n.commandHint_me;
+    case "myroomavatar":
+      return l10n.commandHint_myroomavatar;
+    case "myroomnick":
+      return l10n.commandHint_myroomnick;
+    case "op":
+      return l10n.commandHint_op;
+    case "plain":
+      return l10n.commandHint_plain;
+    case "react":
+      return l10n.commandHint_react;
+    case "send":
+      return l10n.commandHint_send;
+    case "unban":
+      return l10n.commandHint_unban;
+    default:
+      return "";
+  }
+}
---
+++ scripts/generate_command_hints_glue.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Generates some glue code for translation of /command hints.
+
+# How to use this:
+# - Add any new hints to assets/l10n/intl_en.arb
+#   They must be of the form commandHint_<command> with <command> in lowercase.
+# - Run this script to regenerate the glue code
+# - Run flutter test to see if you did everything right
+
+# Looking to add descriptions for a new command, but don't know what it does?
+# It is likely defined here (in registerDefaultCommands()):
+# https://gitlab.com/famedly/company/frontend/famedlysdk/-/blob/main/lib/src/utils/commands_extension.dart
+
+echo "\
+// This file is auto-generated using scripts/generate_command_hints_glue.sh.
+
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+
+String commandHint(L10n l10n, String command) {
+  switch (command) {
+$(sed -n \
+  's/[[:blank:]]*\"\(commandHint_\([[:lower:]]*\)\)\".*/    case "\2":\
+      return l10n.\1;/p' \
+  assets/l10n/intl_en.arb
+)
+    default:
+      return \"\";
+  }
+}\
+" > lib/pages/chat/command_hints.dart
