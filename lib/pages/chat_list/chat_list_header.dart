f289d83a14aa92b8e60e5ddf12005e799ddb3b9a
Christian Pauly <christian-pauly@posteo.de>
2022-07-13 08:40:01 +0200

    chore: Follow up chatlist appbar design

--- lib/pages/chat_list/chat_list_header.dart
+++ lib/pages/chat_list/chat_list_header.dart
@@ -19,7 +19,6 @@ class ChatListHeader extends StatelessWidget implements PreferredSizeWidget {
 
     return AppBar(
       automaticallyImplyLeading: false,
-      scrolledUnderElevation: selectMode == SelectMode.normal ? 0 : null,
       leading: selectMode == SelectMode.normal
           ? null
           : IconButton(
@@ -38,80 +37,67 @@ class ChatListHeader extends StatelessWidget implements PreferredSizeWidget {
                   controller.selectedRoomIds.length.toString(),
                   key: const ValueKey(SelectMode.select),
                 )
-              : Material(
-                  color: Theme.of(context).brightness == Brightness.light
-                      ? Colors.white
-                      : Colors.black,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(AppConfig.borderRadius),
-                    topRight: Radius.circular(AppConfig.borderRadius),
-                  ),
-                  elevation: 4,
-                  shadowColor:
-                      Theme.of(context).colorScheme.onBackground.withAlpha(64),
-                  child: TextField(
-                    controller: controller.searchController,
-                    textInputAction: TextInputAction.search,
-                    onChanged: controller.onSearchEnter,
-                    decoration: InputDecoration(
-                      border: const UnderlineInputBorder(
-                          borderSide: BorderSide.none),
-                      filled: false,
-                      hintText: controller.activeSpacesEntry.getName(context),
-                      prefixIcon: controller.isSearchMode
-                          ? IconButton(
-                              tooltip: L10n.of(context)!.cancel,
-                              icon: const Icon(Icons.close_outlined),
-                              onPressed: controller.cancelSearch,
+              : TextField(
+                  controller: controller.searchController,
+                  textInputAction: TextInputAction.search,
+                  onChanged: controller.onSearchEnter,
+                  decoration: InputDecoration(
+                    border: const UnderlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: controller.activeSpacesEntry.getName(context),
+                    prefixIcon: controller.isSearchMode
+                        ? IconButton(
+                            tooltip: L10n.of(context)!.cancel,
+                            icon: const Icon(Icons.close_outlined),
+                            onPressed: controller.cancelSearch,
+                            color: Theme.of(context).colorScheme.onBackground,
+                          )
+                        : IconButton(
+                            onPressed: Scaffold.of(context).openDrawer,
+                            icon: Icon(
+                              Icons.menu,
                               color: Theme.of(context).colorScheme.onBackground,
-                            )
-                          : IconButton(
-                              onPressed: Scaffold.of(context).openDrawer,
-                              icon: Icon(
-                                Icons.menu,
-                                color:
-                                    Theme.of(context).colorScheme.onBackground,
-                              ),
                             ),
-                      suffixIcon: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: controller.isSearchMode
-                            ? [
-                                if (controller.isSearching)
-                                  const CircularProgressIndicator.adaptive(
-                                    strokeWidth: 2,
-                                  ),
-                                TextButton(
-                                  onPressed: controller.setServer,
-                                  style: TextButton.styleFrom(
-                                    textStyle: const TextStyle(fontSize: 12),
-                                  ),
-                                  child: Text(
-                                    controller.searchServer ??
-                                        Matrix.of(context)
-                                            .client
-                                            .homeserver!
-                                            .host,
-                                    maxLines: 2,
-                                  ),
+                          ),
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: controller.isSearchMode
+                          ? [
+                              if (controller.isSearching)
+                                const CircularProgressIndicator.adaptive(
+                                  strokeWidth: 2,
                                 ),
-                              ]
-                            : [
-                                IconButton(
-                                  icon: Icon(
-                                    Icons.camera_alt_outlined,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onBackground,
-                                  ),
-                                  tooltip: L10n.of(context)!.addToStory,
-                                  onPressed: () =>
-                                      VRouter.of(context).to('/stories/create'),
+                              TextButton(
+                                onPressed: controller.setServer,
+                                style: TextButton.styleFrom(
+                                  textStyle: const TextStyle(fontSize: 12),
                                 ),
-                                ClientChooserButton(controller),
-                                const SizedBox(width: 12),
-                              ],
-                      ),
+                                child: Text(
+                                  controller.searchServer ??
+                                      Matrix.of(context)
+                                          .client
+                                          .homeserver!
+                                          .host,
+                                  maxLines: 2,
+                                ),
+                              ),
+                            ]
+                          : [
+                              IconButton(
+                                icon: Icon(
+                                  Icons.camera_alt_outlined,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onBackground,
+                                ),
+                                tooltip: L10n.of(context)!.addToStory,
+                                onPressed: () =>
+                                    VRouter.of(context).to('/stories/create'),
+                              ),
+                              ClientChooserButton(controller),
+                              const SizedBox(width: 12),
+                            ],
                     ),
                   ),
                 ),
