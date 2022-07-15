542115a1ee9410c8d11fe5a0c519e1ad1a929722
TheOneWithTheBraid <the-one@with-the-braid.cf>
2022-07-15 07:39:28 -0700

    chore: Windows support
    Signed-off-by: TheOneWithTheBraid <the-one@with-the-braid.cf>

--- scripts/prepare-windows.ps1
+++ scripts/prepare-windows.ps1
@@ -1,3 +1,5 @@
 choco install flutter -y
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
+
+flutter config --no-analytics
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus_windows
   desktop_drop
   desktop_lifecycle
-  flutter_secure_storage_windows
   flutter_webrtc
   record_windows
   url_launcher_windows
