0aab896710beb3455b19fb3673a93eadcd7947c9
Christian Pauly <christian-pauly@posteo.de>
2022-09-12 07:49:55 +0200

    chore: Increase default window size on linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -53,7 +53,7 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "FluffyChat");
   }
 
-  gtk_window_set_default_size(window, 800, 600);
+  gtk_window_set_default_size(window, 864, 600);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
