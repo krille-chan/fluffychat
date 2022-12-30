85f4a539d88efbc5b9490ea19f1e2a4c4ad42cb5
Krille <christian-pauly@posteo.de>
2022-12-30 18:24:34 +0100

    chore: Follow up draft fix

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -268,6 +268,7 @@ class ChatController extends State<Chat> {
 
   Future<void> send() async {
     if (sendController.text.trim().isEmpty) return;
+    _storeInputTimeoutTimer?.cancel();
     final prefs = await SharedPreferences.getInstance();
     prefs.remove('draft_$roomId');
     var parseCommands = true;
