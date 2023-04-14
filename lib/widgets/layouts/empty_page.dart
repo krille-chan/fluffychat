a5a3ed4a0df40ed5050575b578b4f01d8c8eb369
Krille <christian-pauly@posteo.de>
2023-04-14 14:24:18 +0200

    style: Make emptypage logo bigger

--- lib/widgets/layouts/empty_page.dart
+++ lib/widgets/layouts/empty_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class EmptyPage extends StatelessWidget {
   final bool loading;
-  static const double _width = 200;
+  static const double _width = 300;
   const EmptyPage({this.loading = false, Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
