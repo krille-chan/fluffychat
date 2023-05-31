e08445ee51f2997b4f51cd35448a38a791a1d2e7
Krille <christian-pauly@posteo.de>
2023-05-31 10:19:13 +0200

    fix: Remove wrong rendered linebreak in html

--- lib/pages/chat/events/html_message.dart
+++ lib/pages/chat/events/html_message.dart
@@ -49,17 +49,20 @@ class HtmlMessage extends StatelessWidget {
     final linkifiedRenderHtml = linkify(
       renderHtml,
       options: const LinkifyOptions(humanize: false),
-    ).map(
-      (element) {
-        if (element is! UrlElement ||
-            element.text.contains('<') ||
-            element.text.contains('>') ||
-            element.text.contains('"')) {
-          return element.text;
-        }
-        return '<a href="${element.url}">${element.text}</a>';
-      },
-    ).join('');
+    )
+        .map(
+          (element) {
+            if (element is! UrlElement ||
+                element.text.contains('<') ||
+                element.text.contains('>') ||
+                element.text.contains('"')) {
+              return element.text;
+            }
+            return '<a href="${element.url}">${element.text}</a>';
+          },
+        )
+        .join('')
+        .replaceAll('\n', '');
 
     final linkColor = textColor.withAlpha(150);
 
