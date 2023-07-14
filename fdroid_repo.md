327ebaa5f8a6fb8c50b10a1d5addf1d6c8b93354
krille-chan <krille-chan@users.noreply.github.com>
2023-07-14 10:15:56 +0000

    deploy: 3c69a8ba6fb66a4555ab7c4fb09e70fd6f08c542

---
+++ fdroid_repo.md
@@ -0,0 +1,56 @@
+# F-Droid Repository
+
+Our own F-Droid repository contains the Google Services which is not possible in the main F-Droid repository. Release candidates
+are also published on it.
+
+## Add Repository to F-Droid
+
+Easiest way to add the Repository is to either **scan the QR-Code** or if you are on your phone **directly click it**.
+
+<a href="fdroidrepos://fluffychat.im/repo/stable/repo/?fingerprint=5EDB5C4395B2F2D9BA682F6A1D275170CCE5365A6FA27D2220EA8D52A6D95F07" >
+    <img src="qr-stable.svg" width="300" height="300"/>
+</a>
+
+
+### If the QR-Code doesn't work:
+
+First check if you have f-droid installed. If not you can get it from: [https://f-droid.org/](https://f-droid.org/).
+After you made sure you installed it and you didn't have it installed before you can try again the QR-Code.
+If this still isn't working follow the next steps:
+
+1. Open the f-droid App
+2. Go to the `Settings` Tab in the Bottom bar
+3. Click the `Repositories` Action
+4. Click on the plus sign at the top.
+5. Fill in `https://fluffychat.im/repo/stable/repo/` into the top field and `5EDB5C4395B2F2D9BA682F6A1D275170CCE5365A6FA27D2220EA8D52A6D95F07` in the bottom field.
+
+## What is the fingerprint?
+
+The fingerprint of the Repository is: `5EDB5C4395B2F2D9BA682F6A1D275170CCE5365A6FA27D2220EA8D52A6D95F07`
+
+# Nightly Repository
+
+## Add Repository to F-Droid
+
+Easiest way to add the Repository is to either **scan the QR-Code** or if you are on your phone **directly click** it.
+
+<a href="fdroidrepos://fluffychat.im/repo/nightly/repo/?fingerprint=21A469657300576478B623DF99D8EB889A80BCD939ACA60A4074741BEAEC397D" >
+    <img src="qr-nightly.svg" width="300" height="300"/>
+</a>
+
+
+### If the QR-Code doesn't work:
+
+First check if you have f-droid installed. If not you can get it from: [https://f-droid.org/](https://f-droid.org/).
+After you made sure you installed it and you didn't have it installed before you can try again the QR-Code.
+If this still isn't working follow the next steps:
+
+1. Open the f-droid App
+2. Go to the `Settings` Tab in the Bottom bar
+3. Click the `Repositories` Action
+4. Click on the plus sign at the top.
+5. Fill in `https://fluffychat.im/repo/nightly/repo/` into the top field and `21A469657300576478B623DF99D8EB889A80BCD939ACA60A4074741BEAEC397D` in the bottom field.
+
+## What is the fingerprint?
+
+The fingerprint of the Repository is: `21A469657300576478B623DF99D8EB889A80BCD939ACA60A4074741BEAEC397D`
---
+++ web/flutter_service_worker.js
@@ -0,0 +1,261 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"index.html": "be07c867d3e1e4e6a139b87d8190050f",
+"/": "be07c867d3e1e4e6a139b87d8190050f",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"favicon.png": "a409751f0ecf6dee76fb350d7402f9be",
+"main.dart.js": "0615046e2671e71010d278a4534cd4c0",
+"version.json": "d237e35c9b9d9622642b37bd264dce63",
+"splash/style.css": "d284d01cad91aeea699e23fe4698ccf1",
+"splash/img/light-4x.png": "e0346148103c17a87682a35525499afe",
+"splash/img/light-2x.png": "9371a9e18df59f2bbe9b32e04c3fc5d4",
+"splash/img/dark-2x.png": "9371a9e18df59f2bbe9b32e04c3fc5d4",
+"splash/img/light-1x.png": "db5b72b7f4b38640c974f20d9c90f464",
+"splash/img/dark-4x.png": "e0346148103c17a87682a35525499afe",
+"splash/img/dark-1x.png": "db5b72b7f4b38640c974f20d9c90f464",
+"splash/img/light-3x.png": "da261be18bbda768fa1462fd8a8cef46",
+"splash/img/dark-3x.png": "da261be18bbda768fa1462fd8a8cef46",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
+"auth.html": "f03ff41ec673844136dfbd81be79aa80",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/fonts/MaterialIcons-Regular.otf": "702e1989d15ce50a009ecaf4c4cdf9e4",
+"assets/fonts/Roboto/RobotoMono-Regular.ttf": "7e173cf37bb8221ac504ceab2acfb195",
+"assets/fonts/Roboto/Roboto-Regular.ttf": "8a36205bd9b83e03af0591a004bc97f4",
+"assets/fonts/Roboto/Roboto-Italic.ttf": "cebd892d1acfcc455f5e52d4104f2719",
+"assets/fonts/Roboto/Roboto-Bold.ttf": "b8e42971dec8d49207a8c8e2b919a6ac",
+"assets/fonts/NotoEmoji/NotoColorEmoji.ttf": "ed84f46d3d5564a08541cd64bddd495c",
+"assets/AssetManifest.bin": "b9118f8e8a0ad8d181d0a70316102217",
+"assets/AssetManifest.json": "612b25e02d4c7e76e7bdcfb2af88fbc3",
+"assets/NOTICES": "033cb1791b1271c0360e6f4ac1bad4b1",
+"assets/FontManifest.json": "1114a5860d5e995c8dc48419a7a6a9e7",
+"assets/packages/wakelock_plus/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Regular.ttf": "dede6f2c7dad4402fa205644391b3a94",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Typewriter-Regular.ttf": "87f56927f1ba726ce0591955c8b3b42d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Caligraphic-Bold.ttf": "a9c8e437146ef63fcd6fae7cf65ca859",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Math-BoldItalic.ttf": "946a26954ab7fbd7ea78df07795a6cbc",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size1-Regular.ttf": "1e6a3368d660edc3a2fbbe72edfeaa85",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Caligraphic-Regular.ttf": "7ec92adfa4fe03eb8e9bfb60813df1fa",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-BoldItalic.ttf": "e3c361ea8d1c215805439ce0941a1c8d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Italic.ttf": "ac3b1882325add4f148f05db8cafd401",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Bold.ttf": "46b41c4de7a936d099575185a94855c4",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size2-Regular.ttf": "959972785387fe35f7d47dbfb0385bc4",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Regular.ttf": "b5f967ed9e4933f1c3165a12fe3436df",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Bold.ttf": "ad0a28f28f736cf4c121bcb0e719b88a",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Italic.ttf": "d89b80e7bdd57d238eeaa80ed9a1013a",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size3-Regular.ttf": "e87212c26bb86c21eb028aba2ac53ec3",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Math-Italic.ttf": "a7732ecb5840a15be39e1eda377bc21d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Regular.ttf": "5a5766c715ee765aa1398997643f1589",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Bold.ttf": "9eef86c1f9efa78ab93d41a0551948f7",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size4-Regular.ttf": "85554307b465da7eb785fd3ce52ad282",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Script-Regular.ttf": "55d2dcd4778875a53ff09320a85a5296",
+"assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "330a6d1961c69dc94875680a8d24268f",
+"assets/packages/handy_window/assets/handy-window.css": "135b89011bc9f52823260e9da2b0c31d",
+"assets/packages/handy_window/assets/handy-window-dark.css": "212d71b05065406d3c6c72c059a4ed9d",
+"assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
+"assets/assets/logo.png": "d329be9cd7af685717f68e03561f96c0",
+"assets/assets/backup.png": "9c1f3ddbb59cee5be2c72ae4eabead1e",
+"assets/assets/banner.png": "4a005db27a8787aea061537223dabb7d",
+"assets/assets/colors.png": "fde0db0023d9fc4b7c96a8114e9329bb",
+"assets/assets/banner_dark.png": "6310493efdf749239c991ec8cf2ac366",
+"assets/assets/login_wallpaper.png": "05f9f8c2f3a51c757f0a7914096b3bdb",
+"assets/assets/favicon.png": "3ea6cdc2aeab08defd0659bad734a69b",
+"assets/assets/js/package/olm_legacy.js": "14d8e3c9941694be62a0fc855cfa4abf",
+"assets/assets/js/package/olm.wasm": "7b6d9296035ed9cd01846571ba244f51",
+"assets/assets/js/package/olm.js": "6791929a85ecc6c063ccd8c0ea2360de",
+"assets/assets/typing.gif": "c64522db9f8f84611d53b0e4ba5fee4e",
+"assets/assets/info-logo.svg": "ccabf48173356d141e75117a97217944",
+"assets/assets/typing.svg": "fac23aaad793ddae0aae89dc7f8bd843",
+"assets/assets/logo.svg": "d042b70cf11a41f2764028e85b07a00a",
+"assets/assets/favicon.ico": "58c583a47977a32782a8706d5a84533c",
+"assets/assets/share.png": "6d8b7e3179bea3d8b7ea1287594289bd",
+"assets/assets/verification.png": "cfafe6d01ed9f2b08312157bc2fd36d3",
+"assets/assets/sounds/call.ogg": "7e8c646f83fba83bfb9084dc1bfec31e",
+"assets/assets/sounds/WoodenBeaver_stereo_message-new-instant.ogg": "88fb9823caeb64edffd343530fae9e8b",
+"assets/assets/sounds/phone.ogg": "5c8fb947eb92ca55229cb6bbf533c40f",
+"assets/assets/chat.svg": "eedd029d2f9f923ffbc09aeb5cf9b1f2",
+"assets/assets/banner_transparent.png": "364e2030f739bf0c7ed1c061c4cb5901",
+"assets/assets/sas-emoji.json": "b9d99fc6dda6a3250af57af969b4a02d",
+"assets/assets/info-logo.png": "9d1d72596564e6639fd984fea2dfd048",
+"icons/Icon-192.png": "839e87c4f6800df757bb28180f8e2949",
+"icons/Icon-512.png": "f57dad4f6efa0339b50d5c65f36dc03c"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
