486a4d4818cd575064a46c27cf929532d254e424
krille-chan <krille-chan@users.noreply.github.com>
2023-07-29 09:10:58 +0000

    deploy: 79c76ab08d048fa9a4ca86b19e086848a23e20ca

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "2c12c3fad0ff5f599263fd06d9c1eaea",
-"/": "2c12c3fad0ff5f599263fd06d9c1eaea",
+const RESOURCES = {"index.html": "87467782ce1925b0c158e6de5d8e5b95",
+"/": "87467782ce1925b0c158e6de5d8e5b95",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "a409751f0ecf6dee76fb350d7402f9be",
 "main.dart.js": "7910f0acda2c05f9b156f59d3d2f74b0",
