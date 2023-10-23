8ef9448703101d82592731526a8ffc8797849591
Krille <christian-pauly@posteo.de>
2023-10-23 15:53:46 +0200

    ci: Change libolm download URL

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ve
 rm -r assets/js/package
-cd assets/js/ && curl -L 'https://gitlab.com/famedly/company/frontend/libraries/olm/-/jobs/artifacts/master/download?job=build:js' > olm.zip && cd ../../
+cd assets/js/ && curl -L $(curl -s 'https://api.github.com/repos/famedly/olm/releases' | jq -r '.[0] | .assets | .[0] | .browser_download_url') > olm.zip && cd ../../
 cd assets/js/ && unzip olm.zip && cd ../../
 cd assets/js/ && rm olm.zip && cd ../../
 cd assets/js/ && mv javascript package && cd ../../
