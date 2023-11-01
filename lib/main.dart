81943f062c8d95a29c2f7963b92c1789316f59e0
krille-chan <krille-chan@posteo.de>
2023-11-01 12:03:27 +0100

    chore: Add better error widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:matrix/matrix.dart';
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/utils/client_manager.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/widgets/error_widget.dart';
 import 'config/setting_keys.dart';
 import 'utils/background_push.dart';
 import 'widgets/fluffy_chat_app.dart';
@@ -63,6 +64,7 @@ Future<void> startGui(List<Client> clients) async {
   await firstClient?.roomsLoading;
   await firstClient?.accountDataLoading;
 
+  ErrorWidget.builder = (details) => FluffyChatErrorWidget(details);
   runApp(FluffyChatApp(clients: clients, pincode: pin));
 }
 
