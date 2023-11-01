eb2310ac41f6df09c7ac42c5d2fd22f409b1d3d5
krille-chan <krille-chan@users.noreply.github.com>
2023-11-01 11:07:24 +0000

    deploy: 81943f062c8d95a29c2f7963b92c1789316f59e0

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "359df14057ebf7a148aead4e8f8dca42",
+const RESOURCES = {"main.dart.js": "4424f4235c0db70827e92ab789e56edc",
 "assets/AssetManifest.json": "4ce8496db0bf33773a61ee0965f7b3fa",
 "assets/assets/banner_dark.png": "6310493efdf749239c991ec8cf2ac366",
 "assets/assets/verification.png": "cfafe6d01ed9f2b08312157bc2fd36d3",
@@ -71,8 +71,8 @@ const RESOURCES = {"main.dart.js": "359df14057ebf7a148aead4e8f8dca42",
 "splash/img/light-3x.png": "da261be18bbda768fa1462fd8a8cef46",
 "splash/img/dark-3x.png": "da261be18bbda768fa1462fd8a8cef46",
 "splash/style.css": "d284d01cad91aeea699e23fe4698ccf1",
-"index.html": "cd2b533928617a99df77f6415eaaec62",
-"/": "cd2b533928617a99df77f6415eaaec62",
+"index.html": "debf40b3680b1603e9ef1f15c698b4ef",
+"/": "debf40b3680b1603e9ef1f15c698b4ef",
 "icons/Icon-512.png": "f57dad4f6efa0339b50d5c65f36dc03c",
 "icons/Icon-192.png": "839e87c4f6800df757bb28180f8e2949",
 "favicon.png": "a409751f0ecf6dee76fb350d7402f9be",
