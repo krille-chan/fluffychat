c7cb83eafb90964eecb5c2300c38a48f1094b406
Vincent Paulin <ecniv81@gmail.com>
2023-12-07 17:39:22 +0100

    feat: alpha tawkie fr on default

--- lib/pages/homeserver_picker/homeserver_app_bar.dart
+++ lib/pages/homeserver_picker/homeserver_app_bar.dart
@@ -13,30 +13,32 @@ class HomeserverAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // List of Tawkie servers (development mode)
+    final List<String> serversTawkie = [
+      'alpha.tawkie.fr',
+      'loveto.party',
+    ];
+
     return Row(
       children: [
         // DropdownButton instead of TypeAheadField
         Expanded(
-          child: DropdownButton<String>(
+          child:
+          DropdownButton<String>(
+            isExpanded: true,
             value: controller.selectedServer,
             icon: const Icon(Icons.arrow_drop_down),
             onChanged: (String? newValue) {
               controller.setSelectedServer(newValue);
             },
-            items: <String>['loveto.party', 'alpha.tawkie', 'matrix.org']
-                .map<DropdownMenuItem<String>>((String value) {
+            items: serversTawkie.map<DropdownMenuItem<String>>((String value) {
               return DropdownMenuItem<String>(
+                alignment: Alignment.center,
                 value: value,
                 child: Text(value),
               );
             }).toList(),
           ),
-        ),
-        IconButton(
-          icon: const Icon(Icons.search),
-          onPressed: () {
-            controller.checkHomeserverAction();
-          },
         ),
       ],
     );
