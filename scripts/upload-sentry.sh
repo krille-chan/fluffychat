4946ffea23e1dff980d99fd1477d18704bff2ab5
Brord van Wierst <brordvwierst@gmail.com>
2023-12-20 02:59:54 +0100

    Gh actions improvements (#20)
    Fixed sentry

--- scripts/upload-sentry.sh
+++ scripts/upload-sentry.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -ve
 
 # Build a release version of the app for a platform and upload symbols
-OUTPUT_FOLDER_WEB=./build/web/
-SENTRY_RELEASE=$CI_COMMIT_SHA
-SENTRY_PROJECT="client"
-SENTRY_ORG="pangea-chat"
+export OUTPUT_FOLDER_WEB=./build/web/
+export SENTRY_RELEASE=$CI_COMMIT_SHA
+export SENTRY_PROJECT="${SENTRY_ORG:-client}"
+export SENTRY_ORG="${SENTRY_ORG:-pangea-chat}"
 
 echo "[run] Uploading sourcemaps for $SENTRY_RELEASE"
-sentry-cli releases new $SENTRY_RELEASE
+echo "[run] $SENTRY_PROJECT @ $SENTRY_ORG / $OUTPUT_FOLDER_WEB"
+sentry-cli releases new $SENTRY_RELEASE --org $SENTRY_ORG
 sentry-cli releases set-commits $CI_COMMIT_SHA --auto
 sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps . \
     --ext dart \
