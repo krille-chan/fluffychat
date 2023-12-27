6f9e81f0b22ee05bb9dd98099e45c08cc43ca002
krille-chan <krille-chan@users.noreply.github.com>
2023-12-27 17:19:26 +0000

    deploy: 7c07d2b85b8bb1136fc7734e1e88fa63c6952df0

--- nightly/flutter_service_worker.js
+++ nightly/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
-"index.html": "1468efdffe47ad38645614f6627eb9aa",
-"/": "1468efdffe47ad38645614f6627eb9aa",
+"index.html": "a2ef45a94ed762c23a1568e4967fbc74",
+"/": "a2ef45a94ed762c23a1568e4967fbc74",
 "assets/AssetManifest.bin": "7deace30ec5096ee9644e4f212355864",
 "assets/fonts/NotoEmoji/NotoColorEmoji.ttf": "ed84f46d3d5564a08541cd64bddd495c",
 "assets/fonts/Roboto/Roboto-Bold.ttf": "b8e42971dec8d49207a8c8e2b919a6ac",
