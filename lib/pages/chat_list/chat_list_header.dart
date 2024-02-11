bc68a4faaa9e85b21692256ebfb8c31702ce4009
Marcus Hoffmann <bubu@bubu1.eu>
2024-02-11 14:08:45 +0100

    fix: use correct icons for chat pin/unpin
    Similarly to the read/unread toggle this was also using the wrong icon.
    
    If any selected room is not favourite, then show the pin icon (which
    should represent the "mark as favourite" action as it corresponds to the
    filled pin shown next to a favourited room. Otherwise show the outline
    icon, representing the "unpin" action.

--- lib/pages/chat_list/chat_list_header.dart
+++ lib/pages/chat_list/chat_list_header.dart
@@ -147,8 +147,8 @@ class ChatListHeader extends StatelessWidget implements PreferredSizeWidget {
                     tooltip: L10n.of(context)!.toggleFavorite,
                     icon: Icon(
                       controller.anySelectedRoomNotFavorite
-                          ? Icons.push_pin_outlined
-                          : Icons.push_pin,
+                          ? Icons.push_pin
+                          : Icons.push_pin_outlined,
                     ),
                     onPressed: controller.toggleFavouriteRoom,
                   ),
