abbcd41064d98dda9c4b32f4c41d253887f2ec5f
Gabby Gurdin <ggurdin@gmail.com>
2024-02-22 11:25:27 -0500

    toolbar updates

--- lib/pangea/widgets/igc/word_data_card.dart
+++ lib/pangea/widgets/igc/word_data_card.dart
@@ -381,3 +381,20 @@ class PartOfSpeechBlock extends StatelessWidget {
     );
   }
 }
+
+class SelectToDefine extends StatelessWidget {
+  const SelectToDefine({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Text(
+        L10n.of(context)!.selectToDefine,
+        style: BotStyle.text(context),
+      ),
+    );
+  }
+}
