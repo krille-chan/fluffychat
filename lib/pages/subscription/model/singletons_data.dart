d44a76991a3198ecbc06a2920eca503ec38e2451
Emmanuelle Duchemann <ecniv81@gmail.com>
2024-03-13 18:36:19 +0100

    feat: paywall update

---
+++ lib/pages/subscription/model/singletons_data.dart
@@ -0,0 +1,13 @@
+class AppData {
+  static final AppData _appData = AppData._internal();
+
+  bool entitlementIsActive = false;
+  String appUserID = '';
+
+  factory AppData() {
+    return _appData;
+  }
+  AppData._internal();
+}
+
+final appData = AppData();
---
+++ lib/pages/subscription/model/style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// UI Colors
+const kColorText = Colors.white;
+const kColorNavIcon = Color.fromRGBO(131, 136, 139, 1.0);
+const kColorBackground = Color.fromRGBO(30, 28, 33, 1.0);
+
+// Text Styles
+const kFontSizeSuperSmall = 10.0;
+const kFontSizeNormal = 16.0;
+const kFontSizeMedium = 18.0;
+const kFontSizeLarge = 96.0;
+
+const kDescriptionTextStyle = TextStyle(
+  color: kColorText,
+  fontWeight: FontWeight.normal,
+  fontSize: kFontSizeNormal,
+);
+
+const kTitleTextStyle = TextStyle(
+  color: kColorText,
+  fontWeight: FontWeight.bold,
+  fontSize: kFontSizeMedium,
+);
+
+// Inputs
+const kButtonRadius = 10.0;
