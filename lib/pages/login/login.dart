81d3587a9680778f9e19c817759293bf9dd677a5
Paul ALNET <paul@shimaore.net>
2024-03-14 08:51:53 +0100

    chore(login): update api baseUrls

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -36,7 +36,8 @@ class LoginController extends State<Login> {
   bool loading = false;
   bool showPassword = false;
   final Dio dio =
-      Dio(BaseOptions(baseUrl: 'https://staging.tawkie.fr/panel/api/.ory'));
+      Dio(BaseOptions(baseUrl: 'https://tawkie.fr/panel/api/.ory'));
+  // TODO if debug build, use staging
 
   final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
 
@@ -137,8 +138,9 @@ class LoginController extends State<Login> {
     setState(() => loading = true);
 
     try {
+      // TODO use baseUrl
       final responseMatrix = await dio.get(
-        'https://staging.tawkie.fr/panel/api/mobile-matrix-auth/getMatrixToken',
+        'https://tawkie.fr/panel/api/mobile-matrix-auth/getMatrixToken',
         options: Options(
           headers: {
             'X-Session-Token': sessionToken,
@@ -150,6 +152,15 @@ class LoginController extends State<Login> {
 
       final String matrixLoginJwt = responseData['matrixLoginJwt'];
       final String serverName = responseData['serverName'];
+
+      // TODO handle errors correctly if not caught and displayed
+      if (!matrixLoginJwt.startsWith('ey')) {
+        throw Exception('Server did not return a valid JWT');
+      }
+
+      if (serverName.isEmpty) {
+        throw Exception('Server did not return a valid server name');
+      }
 
       var homeserver = Uri.parse(serverName);
       if (homeserver.scheme.isEmpty) {
@@ -165,7 +176,8 @@ class LoginController extends State<Login> {
         );
       }
 
-      const url = 'https://matrix.staging.tawkie.fr/_matrix/client/r0/login';
+      // TODO use homeserver variable
+      final url = 'https://' + serverName + '/_matrix/client/r0/login';
       final headers = {'Content-Type': 'application/json'};
       final data = {'type': 'org.matrix.login.jwt', 'token': matrixLoginJwt};
 
