6347273ec4876247dedcdb7e9a61f520abc100f9
Vincent Paulin <ecniv81@gmail.com>
2024-03-28 10:48:40 +0100

    feat: change username function message

--- lib/pages/login/change_username_page.dart
+++ lib/pages/login/change_username_page.dart
@@ -34,24 +34,28 @@ class _ChangeUsernamePageState extends State<ChangeUsernamePage> {
       );
       final matrixUsername = updateUsernameResponse.data['username'];
 
-      print("la reponse est: $updateUsernameResponse");
-      print("matrixUsername: $matrixUsername");
+      Logs().v("New matrixUsername: $matrixUsername");
       if (matrixUsername != newUsername) {
         throw Exception('Error during username update');
       }
+
+      setState(() {
+        widget.queueStatus['username'] = newUsername;
+      });
+
+      // Display a SnackBar to indicate a successful name change
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Changement de nom réussi pour $newUsername'),
+          backgroundColor: Colors.green,
+        ),
+      );
     } on DioException catch (e) {
       if (kDebugMode) {
         print("Exception when calling Kratos log: $e\n");
       }
       Logs().v("Error Kratos login : ${e.response?.data}");
-
-      // Display Kratos error messages to the user
-      if (e.response!.data['ui']['messages'][0]['text'] != null) {
-        final errorMessage = e.response!.data['ui']['messages'][0]['text'];
-        setState(() => _usernameError = errorMessage);
-      } else {
-        setState(() => _usernameError = "Dio error with Kratos");
-      }
+      setState(() => _usernameError = e.response?.data['message']);
     } catch (exception) {
       setState(() => _usernameError = exception.toString());
       throw Exception('Error during username update');
@@ -76,81 +80,88 @@ class _ChangeUsernamePageState extends State<ChangeUsernamePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-              child: Image.asset(
-                'assets/logo.png',
-                width: 100,
-                height: 100,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 100,
+                  height: 100,
+                ),
               ),
-            ),
-            widget.queueStatus['userState'] == 'IN_QUEUE'
-                ? Column(
-                    children: [
-                      Text(
-                        L10n.of(context)!.username_yourPosition,
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                      Text(
-                        widget.queueStatus['queuePosition'].toString(),
-                        style: const TextStyle(fontSize: 23),
-                      ),
-                    ],
-                  )
-                : Text(
-                    L10n.of(context)!.username_itsYourTurn,
-                    style: const TextStyle(fontSize: 23),
-                  ),
-            const SizedBox(height: 10),
-            Text(
-              L10n.of(context)!.username_changeUsername,
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: L10n.of(context)!.username,
-                errorText: _usernameError,
+              widget.queueStatus['userState'] == 'IN_QUEUE'
+                  ? Column(
+                      children: [
+                        Text(
+                          L10n.of(context)!.username_yourPosition,
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          widget.queueStatus['queuePosition'].toString(),
+                          style: const TextStyle(fontSize: 23),
+                        ),
+                      ],
+                    )
+                  : Text(
+                      L10n.of(context)!.username_itsYourTurn,
+                      style: const TextStyle(fontSize: 23),
+                    ),
+              const SizedBox(height: 10),
+              Text(
+                L10n.of(context)!.username_changeUsername,
+                style: const TextStyle(fontSize: 18),
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return L10n.of(context)!.register_requiredField;
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            widget.queueStatus['username'] != null &&
-                    widget.queueStatus['username'] != ""
-                ? Text(
-                    L10n.of(context)!.username_advertisement,
-                  )
-                : Container(),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final newUsername = _usernameController.text;
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: L10n.of(context)!.username,
+                  errorText: _usernameError,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return L10n.of(context)!.register_requiredField;
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              widget.queueStatus['username'] != null &&
+                      widget.queueStatus['username'] != ""
+                  ? Text(
+                      L10n.of(context)!.username_advertisement,
+                    )
+                  : Container(),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  if (widget.queueStatus['username'] !=
+                      _usernameController.text) {
+                    final newUsername = _usernameController.text;
 
-                if (_usernameController.text.isEmpty) {
-                  setState(() {
-                    _usernameError = L10n.of(context)!.register_requiredField;
-                  });
-                  return;
-                } else {
-                  setState(() {
-                    _usernameError = null;
-                  });
+                    if (_usernameController.text.isEmpty) {
+                      setState(() {
+                        _usernameError =
+                            L10n.of(context)!.register_requiredField;
+                      });
+                      return;
+                    } else {
+                      setState(() {
+                        _usernameError = null;
+                      });
 
-                  // Update user name
-                  await updateUsername(widget.sessionToken, newUsername);
-                }
-              },
-              child: Text(L10n.of(context)!.submit),
-            ),
-          ],
+                      // Update user name
+                      await updateUsername(widget.sessionToken, newUsername);
+                    }
+                  }
+                },
+                child: Text(L10n.of(context)!.submit),
+              ),
+            ],
+          ),
         ),
       ),
     );
