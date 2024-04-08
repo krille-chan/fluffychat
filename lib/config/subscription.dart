970c9df345b8da491b5963bab0e8343f2e676ce6
Vincent Paulin <ecniv81@gmail.com>
2024-04-08 17:17:25 +0200

    feat: sub redirect

--- lib/config/subscription.dart
+++ lib/config/subscription.dart
@@ -4,37 +4,6 @@ import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:purchases_ui_flutter/purchases_ui_flutter.dart';
 
 class SubscriptionManager {
-  static bool _isSubscribed = false;
-
-  // Static method for initializing and listening for subscription changes
-  static void initAndListen() {
-    // Initialize subscription status
-    _checkSubscriptionStatus();
-
-    // Listen to subscription status updates
-    Purchases.addCustomerInfoUpdateListener((purchaserInfo) {
-      _checkSubscriptionStatus();
-    });
-  }
-
-  // Private method for checking subscription status
-  static void _checkSubscriptionStatus() {
-    // Check if the user is a subscriber
-    Purchases.getCustomerInfo().then((purchaserInfo) {
-      if (purchaserInfo.entitlements.active.containsKey("tawkie_sub")) {
-        _isSubscribed = true;
-      } else {
-        _isSubscribed = false;
-      }
-    });
-  }
-
-  // Static method to check if the user is a subscriber
-  static bool isSubscribed() {
-    // Resend subscription status
-    return _isSubscribed;
-  }
-
   void presentPaywall() async {
     final paywallResult = await RevenueCatUI.presentPaywall();
     log('Paywall result: $paywallResult');
@@ -64,12 +33,9 @@ class SubscriptionManager {
   }
 
   void checkSubscriptionStatusAndRedirect() async {
-    CustomerInfo customerInfo = await Purchases.getCustomerInfo();
+    bool hasSub = await checkSubscriptionStatus();
 
-    if (customerInfo.entitlements.all['tawkie_sub'] != null &&
-        customerInfo.entitlements.all['tawkie_sub']?.isActive == true) {
-      // Connect to Matrix
-    } else {
+    if (!hasSub) {
       SubscriptionManager().presentPaywall();
     }
   }
