31c8b181c6a6686a523d77fe23cf1d78c4f8ffc0
Vincent Paulin <ecniv81@gmail.com>
2024-04-30 09:47:11 +0200

    feat: reg ex pattern update

--- lib/pages/add_bridge/service/reg_exp_pattern.dart
+++ lib/pages/add_bridge/service/reg_exp_pattern.dart
@@ -2,37 +2,18 @@
 class PingPatterns {
   // Instagram
   static RegExp instagramOnlineMatch = RegExp(r"MQTT connection is active");
-  static RegExp instagramMQTTNotMatch = RegExp(r"MQTT not connected");
-  static RegExp instagramSuccessfullyMatch = RegExp(r"Successfully logged in");
-  static RegExp instagramAlreadySuccessMatch =
-      RegExp(r"You're already logged in");
-  static RegExp instagramSyncComplete = RegExp(r"Synchronization complete");
-  static RegExp instagramNotLoggedMatch =
-      RegExp(r"You're not logged into Instagram");
-  static RegExp instagramDisconnectMatch = RegExp(r"Successfully logged out");
+  static RegExp instagramNotLoggedMatch = RegExp(r"You're not logged in");
 
   // WhatsApp
   static RegExp whatsAppOnlineMatch = RegExp(r"connection to WhatsApp OK");
-  static RegExp whatsAppSuccessfullyMatch = RegExp(r"Successfully logged in");
-  static RegExp whatsAppAlreadySuccessMatch =
-      RegExp(r"You're already logged in");
   static RegExp whatsAppNotLoggedMatch =
       RegExp(r"You're not logged into WhatsApp");
-  static RegExp whatsAppDisconnectMatch = RegExp(r"Logged out successfully");
-  static RegExp whatsAppConnectedButNotLoggedMatch =
-      RegExp(r"Connected to WhatsApp, but not logged in");
   static RegExp whatsAppLoggedButNotConnectedMatch =
       RegExp(r"but you don't have a WhatsApp connection.");
 
   // Facebook Messenger
-  static RegExp facebookOnlineMatch =
-      RegExp(r"The Messenger MQTT listener is connected.");
-  static RegExp facebookMQTTNotMatch =
-      RegExp(r"You don't have a Messenger MQTT connection.");
-  static RegExp facebookSuccessfullyMatch = RegExp(r"You're logged in as");
-  static RegExp facebookNotLoggedMatch =
-      RegExp(r"That command requires you to be logged in.");
-  static RegExp facebookDisconnectMatch = RegExp(r"Successfully logged out");
+  static RegExp facebookOnlineMatch = RegExp(r".*You're logged into Meta");
+  static RegExp facebookNotLoggedMatch = RegExp(r"You're not logged in");
 }
 
 // For login response
@@ -67,18 +48,11 @@ class LoginRegex {
       RegExp(r"Login timed out. Please restart the login");
 
   // Facebook
-  static final RegExp facebookSuccessMatch = RegExp(r"Successfully logged in");
-  static final RegExp facebookSendPasswordMatch =
-      RegExp(r"Please send your password here to log in");
-  static final RegExp facebookTwoFactorMatch =
-      RegExp(r"You have two-factor authentication turned on.");
-  static final RegExp facebookNameOrPasswordErrorMatch =
-      RegExp(r"Invalid username or password");
-  static final RegExp facebookRateLimitErrorMatch = RegExp(r"rate_limit_error");
+  static final RegExp facebookSuccessMatch = RegExp(r".*Successfully logged");
   static final RegExp facebookAlreadyConnectedMatch =
       RegExp(r"You're already logged in");
-  static final RegExp facebookIncorrectTwoFactorMatch =
-      RegExp(r"Incorrect two-factor authentication code. Please try again");
+  static final RegExp facebookPasteCookies =
+      RegExp(r'^.*Paste your cookies here.*');
 }
 
 // For logout response
@@ -96,7 +70,7 @@ class LogoutRegex {
       RegExp(r"You're not logged in.");
 
   // Facebook Messenger
-  static final RegExp facebookSuccessMatch = RegExp(r"Successfully logged out");
+  static final RegExp facebookSuccessMatch = RegExp(r".*Disconnected");
   static final RegExp facebookAlreadyLogoutMatch =
       RegExp(r"That command requires you to be logged in.");
 }
