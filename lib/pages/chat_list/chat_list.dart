0815d480a932cfb20e338f84f8c32e7226f0c734
Shubham <shubhamkumar101100@gmail.com>
2024-05-25 19:01:34 +0530

    chore: Remove unused import of flutter/services.dart

--- lib/pages/chat_list/chat_list.dart
+++ lib/pages/chat_list/chat_list.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
