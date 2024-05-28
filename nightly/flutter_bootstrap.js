84c783ef729d26f68513d9d0c21c740b60ee3d46
krille-chan <krille-chan@users.noreply.github.com>
2024-05-28 11:00:48 +0000

    deploy: 5829446afa580793ad54dc3055776c6ba5052649

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1844841979"
+    serviceWorkerVersion: "4108386266"
   }
 });
