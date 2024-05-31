5345e4a6249842f21141a1f8f36aea10fc39e046
Vincent Paulin <ecniv81@gmail.com>
2024-05-31 13:58:28 +0200

    feat: messenger user agent only

--- lib/pages/add_bridge/web_view_connection.dart
+++ lib/pages/add_bridge/web_view_connection.dart
@@ -46,12 +46,17 @@ class _WebViewConnectionState extends State<WebViewConnection> {
     final connectionState =
         Provider.of<ConnectionStateModel>(context, listen: false);
 
+    InAppWebViewSettings settings = InAppWebViewSettings();
+    if (widget.network.name == "Facebook Messenger") {
+      settings = InAppWebViewSettings(
+        userAgent:
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      );
+    }
+
     return Scaffold(
       body: InAppWebView(
-        initialSettings: InAppWebViewSettings(
-          userAgent:
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        ),
+        initialSettings: settings,
         initialUrlRequest: URLRequest(
           url: WebUri(widget.network.urlLogin!),
         ),
