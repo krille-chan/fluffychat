9d2b4882133dd87ecf550db99ab1990986b336e6
Vincent Paulin <ecniv81@gmail.com>
2024-06-06 11:13:37 +0200

    fix remove import

--- lib/pages/add_bridge/add_bridge_body.dart
+++ lib/pages/add_bridge/add_bridge_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:tawkie/pages/add_bridge/add_bridge.dart';
-import 'package:tawkie/pages/add_bridge/social_network_action.dart';
 import 'package:tawkie/pages/add_bridge/social_network_item.dart';
 import 'package:tawkie/utils/platform_infos.dart';
 import 'package:tawkie/utils/platform_size.dart';
