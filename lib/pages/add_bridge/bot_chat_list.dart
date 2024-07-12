12d3a1917f94f7986c45fa9a01aef094cd2366f7
Vincent Paulin <ecniv81@gmail.com>
2024-07-12 12:11:06 +0200

    feat: add leave funtion in chat bot list

--- lib/pages/add_bridge/bot_chat_list.dart
+++ lib/pages/add_bridge/bot_chat_list.dart
@@ -1,35 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:future_loading_dialog/future_loading_dialog.dart';
 import 'package:matrix/matrix.dart';
 import 'package:tawkie/pages/chat/chat.dart';
 import 'package:tawkie/pages/chat_list/chat_list_item.dart';
 import 'package:tawkie/widgets/matrix.dart';
 
-class BotChatListPage extends StatelessWidget {
+class BotChatListPage extends StatefulWidget {
   final List<String> botUserIds;
 
   const BotChatListPage({super.key, required this.botUserIds});
 
   @override
-  Widget build(BuildContext context) {
-    final List<Room> rooms = Matrix.of(context)
-        .client
-        .rooms
-        .where((room) => botUserIds.contains(room.directChatMatrixID))
-        .toList();
+  State<BotChatListPage> createState() => _BotChatListPageState();
+}
 
+class _BotChatListPageState extends State<BotChatListPage> {
+  Future<List<Room>>? _roomsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRooms();
+  }
+
+  void _loadRooms() {
+    setState(() {
+      _roomsFuture = Future.delayed(
+        Duration.zero,
+        () => Matrix.of(context)
+            .client
+            .rooms
+            .where(
+                (room) => widget.botUserIds.contains(room.directChatMatrixID))
+            .toList(),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Conversations avec les bots'),
       ),
-      body: ListView.builder(
-        itemCount: rooms.length,
-        itemBuilder: (context, i) {
-          final room = rooms[i];
-          return ChatListItem(rooms[i],
-              key: Key('chat_list_item_${rooms[i].id}'), onTap: () {
-            // Handle tap on a bot conversation
-            _onChatTap(room, context);
-          });
+      body: FutureBuilder<List<Room>>(
+        future: _roomsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erreur: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('Aucune conversation avec les bots.'));
+          } else {
+            final rooms = snapshot.data!;
+            return ListView.builder(
+              itemCount: rooms.length,
+              itemBuilder: (context, i) {
+                final room = rooms[i];
+                return ChatListItem(rooms[i],
+                    key: Key('chat_list_item_${rooms[i].id}'), onTap: () {
+                  // Handle tap on a bot conversation
+                  _onChatTap(room, context);
+                }, onLongPress: () {
+                  // Handle long press to delete the room
+                  _onChatLongPress(room, context);
+                });
+              },
+            );
+          }
         },
       ),
     );
@@ -44,5 +84,59 @@ class BotChatListPage extends StatelessWidget {
                 roomId: room.id,
               )),
     );
+  }
+
+  // Method to handle chat long press
+  void _onChatLongPress(Room room, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Quitter la conversation'),
+          content: Text('Voulez-vous vraiment quitter cette conversation ?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text(L10n.of(context)!.cancel),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(L10n.of(context)!.leave),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                await showFutureLoadingDialog(
+                    context: context, future: () => _leaveRoom(room));
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Method to leave the room
+  Future<void> _leaveRoom(Room room) async {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      await room.leave();
+      await Future.delayed(
+          const Duration(seconds: 2)); // Delay to ensure the room is left
+      // Reload rooms after leaving one
+      _loadRooms();
+      scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content: Text('Vous avez quitté la conversation avec succès.'),
+        ),
+      );
+    } catch (e) {
+      scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content:
+              Text('Erreur lors de la tentative de quitter la conversation.'),
+        ),
+      );
+    }
   }
 }
