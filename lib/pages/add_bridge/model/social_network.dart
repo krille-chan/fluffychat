465271f6d56a4f4233ef6b8b51bf56baba3408cf
Paul ALNET <paul@shimaore.net>
2024-07-22 20:39:00 +0200

    feat: add network chat info

--- lib/pages/add_bridge/model/social_network.dart
+++ lib/pages/add_bridge/model/social_network.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:tawkie/config/themes.dart';
 
 class SocialNetwork {
   final Widget logo; // The path to social media image
+  final String chatIconPath; // The path to chat icon asset
+  final Color color; // The color of the social media displayed in chat list
   final String name; // Social media name
   final String chatBot; // ChatBot for send demand
+  final String mxidPrefix; // The matrix ID prefix used to identify puppets
+  final String
+      displayNameSuffix; // The `(Network)` suffix to remove from displayname)
   final String? urlLogin;
   final String? urlRedirect;
   bool? loading; // To find out if state is loading
@@ -13,8 +19,12 @@ class SocialNetwork {
 
   SocialNetwork({
     required this.logo,
+    required this.chatIconPath,
+    required this.color,
     required this.name,
     required this.chatBot,
+    required this.mxidPrefix,
+    this.displayNameSuffix = "",
     this.urlLogin,
     this.urlRedirect,
     this.loading = true, // Default value true for loading
@@ -44,31 +54,47 @@ class WhatsAppResult {
   WhatsAppResult(this.result, this.code, this.qrCode);
 }
 
-class SocialNetworkManager{
+class SocialNetworkManager {
   static final List<SocialNetwork> socialNetworks = [
     SocialNetwork(
       logo: Logo(Logos.facebook_messenger),
+      chatIconPath: "assets/facebook-messenger.png",
+      color: FluffyThemes.facebookColor,
       name: "Facebook Messenger",
       chatBot: "@messenger2bot:",
+      displayNameSuffix: "(FB)",
+      mxidPrefix: "@messenger2_",
       urlLogin: "https://www.messenger.com/",
       urlRedirect: "https://www.messenger.com/t/",
     ),
     SocialNetwork(
       logo: Logo(Logos.instagram),
+      chatIconPath: "assets/instagram.png",
+      color: FluffyThemes.instagramColor,
       name: "Instagram",
       chatBot: "@instagram2bot:",
+      displayNameSuffix: "(IG)",
+      mxidPrefix: "@instagram2_",
       urlLogin: "https://www.instagram.com/accounts/login/",
       urlRedirect: "https://www.instagram.com/",
     ),
     SocialNetwork(
       logo: Logo(Logos.whatsapp),
+      chatIconPath: "assets/whatsapp.png",
+      color: FluffyThemes.whatsAppColor,
       name: "WhatsApp",
       chatBot: "@whatsappbot:",
+      displayNameSuffix: "(WhatsApp)",
+      mxidPrefix: "@whatsapp_",
     ),
     SocialNetwork(
       logo: Logo(Logos.linkedin),
+      chatIconPath: "assets/linkedin.png",
       name: "Linkedin",
+      color: FluffyThemes.linkedinColor,
       chatBot: "@linkedinbot:",
+      displayNameSuffix: "(LinkedIn)",
+      mxidPrefix: "@linkedin_",
       urlLogin: "https://www.linkedin.com/login/",
       urlRedirect: "https://www.linkedin.com/feed/",
     ),
