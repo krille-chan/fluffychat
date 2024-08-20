5c7a1f554b113df86c6b9b1b09e2eb89a2c04f87
ggurdin <ggurdin@gmail.com>
2024-08-20 14:48:44 -0400

    added tooltip toggle status to matrix profile to prevent showing again after it's been dismissed

--- lib/pangea/enum/instructions_enum.dart
+++ lib/pangea/enum/instructions_enum.dart
@@ -72,4 +72,17 @@ extension InlineInstructionsExtension on InlineInstructions {
         return L10n.of(context)!.translationChoicesBody;
     }
   }
+
+  bool get toggledOff {
+    final instructionSettings =
+        MatrixState.pangeaController.userController.profile.instructionSettings;
+    switch (this) {
+      case InlineInstructions.speechToText:
+        return instructionSettings.showedSpeechToTextTooltip;
+      case InlineInstructions.l1Translation:
+        return instructionSettings.showedL1TranslationTooltip;
+      case InlineInstructions.translationChoices:
+        return instructionSettings.showedTranslationChoicesTooltip;
+    }
+  }
 }
