bd41aeb45ec48b94a5950542076820809a7cd419
KaMeHb-UA <marlock@duck.com>
2024-08-24 12:21:51 +0300

    fix: use raw `yq` query result (formatted one may result in `"d.d.d"` string on some systems)

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -3,7 +3,7 @@ rm -r assets/js/package
 mkdir -p assets/js/package
 touch assets/js/package/.gitkeep
 
-OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm)
+OLM_VERSION=$(cat pubspec.yaml | yq -r .dependencies.flutter_olm)
 DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
 
 cd assets/js/ && curl -L $DOWNLOAD_PATH > olm.zip && cd ../../
