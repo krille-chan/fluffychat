8ba46a3533df8c66dc6b4115d56c6d0026cd14ad
krille-chan <christian-kussowski@posteo.de>
2024-11-03 08:20:05 +0100

    chore: Nicer empty page

--- lib/widgets/layouts/empty_page.dart
+++ lib/widgets/layouts/empty_page.dart
@@ -8,6 +8,7 @@ class EmptyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = min(MediaQuery.of(context).size.width, EmptyPage._width) / 2;
+    final theme = Theme.of(context);
     return Scaffold(
       // Add invisible appbar to make status bar on Android tablets bright.
       appBar: AppBar(
@@ -19,7 +20,8 @@ class EmptyPage extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: Image.asset(
-          'assets/info-logo.png',
+          'assets/logo_transparent.png',
+          color: theme.colorScheme.surfaceContainerHigh,
           width: width,
           height: width,
           filterQuality: FilterQuality.medium,
