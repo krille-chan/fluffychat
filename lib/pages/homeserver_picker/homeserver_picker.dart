a2e7356bd0c3219e75460e319cabaed28335d8a6
Krille <c.kussowski@famedly.com>
2024-11-04 14:06:07 +0100

    chore: Follow up homeserver picker

--- lib/pages/homeserver_picker/homeserver_picker.dart
+++ lib/pages/homeserver_picker/homeserver_picker.dart
@@ -80,6 +80,7 @@ class HomeserverPickerController extends State<HomeserverPicker> {
   }
 
   void onSubmitted([_]) {
+    if (isLoading) return tryCheckHomeserverActionWithoutCooldown();
     if (supportsSso) return ssoLoginAction();
     if (supportsPasswordLogin) return login();
     return tryCheckHomeserverActionWithoutCooldown();
