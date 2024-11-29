8f483c64c025bffcd918ae1e802f0dfece17322f
krille-chan <christian-kussowski@posteo.de>
2024-11-29 19:12:33 +0100

    build: Use file selector to save files

--- lib/utils/matrix_sdk_extensions/matrix_file_extension.dart
+++ lib/utils/matrix_sdk_extensions/matrix_file_extension.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:file_selector/file_selector.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:matrix/matrix.dart';
 import 'package:share_plus/share_plus.dart';
@@ -19,21 +20,18 @@ extension MatrixFileExtension on MatrixFile {
       return;
     }
 
-    final downloadPath = await FilePicker.platform.saveFile(
-      dialogTitle: L10n.of(context).saveFile,
-      fileName: name,
-      type: filePickerFileType,
-      bytes: bytes,
+    final location = await getSaveLocation(
+      suggestedName: name,
+      confirmButtonText: L10n.of(context).saveFile,
     );
+    final downloadPath = location?.path;
     if (downloadPath == null) return;
 
-    if (PlatformInfos.isDesktop) {
-      final result = await showFutureLoadingDialog(
-        context: context,
-        future: () => File(downloadPath).writeAsBytes(bytes),
-      );
-      if (result.error != null) return;
-    }
+    final result = await showFutureLoadingDialog(
+      context: context,
+      future: () => File(downloadPath).writeAsBytes(bytes),
+    );
+    if (result.error != null) return;
 
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
