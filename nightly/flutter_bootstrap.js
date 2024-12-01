02e21b24383305ed6e7b260c2b7bace148433b90
krille-chan <krille-chan@users.noreply.github.com>
2024-12-01 19:45:21 +0000

    deploy: d182e1ecea01bc73e9e6ff46afd0ed3a58e87479

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3569448942"
+    serviceWorkerVersion: "2420472824"
   }
 });
--- nightly/flutter_service_worker.js
+++ nightly/flutter_service_worker.js
@@ -18,7 +18,7 @@ const RESOURCES = {"main.dart.js_289.part.js": "41ce03aa2f47af14357a341b4956a3a2
 "main.dart.js_259.part.js": "e37431b2058ef41b516cc61117c0f85e",
 "main.dart.js_207.part.js": "893fa82c54f215ef710e676134a1cc9c",
 "main.dart.js_278.part.js": "bdba14dfdd662803442c81ab66e70694",
-"main.dart.js": "2d923e1ac1604452eb174c501db9995c",
+"main.dart.js": "5d5cc764c244122f519dd566bd246415",
 "main.dart.js_293.part.js": "b0a92f3045f01daf956dfea8dde9f660",
 "main.dart.js_192.part.js": "a2002c6f4313c768040d0e7f329f93df",
 "main.dart.js_275.part.js": "315effccaf612de2f77a9839d76ac5ad",
@@ -93,8 +93,8 @@ const RESOURCES = {"main.dart.js_289.part.js": "41ce03aa2f47af14357a341b4956a3a2
 "main.dart.js_280.part.js": "04b9f127f38119ef3cd13c3183115f5b",
 "main.dart.js_272.part.js": "3dbb2e058023857966bd41fe5356c659",
 "main.dart.js_242.part.js": "db3c881b180c01f92cd5167a91d85aec",
-"index.html": "09a7956192ce1068d38dff4d613749e5",
-"/": "09a7956192ce1068d38dff4d613749e5",
+"index.html": "94676ea1d145cf1e70bd5b30720f8256",
+"/": "94676ea1d145cf1e70bd5b30720f8256",
 "main.dart.js_235.part.js": "da0d10daa3d0f469f6766b304d9bc184",
 "manifest.json": "cc4b6aa791018840b65fd0b0e325b201",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
@@ -133,7 +133,7 @@ const RESOURCES = {"main.dart.js_289.part.js": "41ce03aa2f47af14357a341b4956a3a2
 "main.dart.js_226.part.js": "b8cfa72b06691a5063cb0635412378f4",
 "main.dart.js_14.part.js": "9648d34764ff46509af543f0e5adc50e",
 "version.json": "e9d913db2fdc769aa5e6581dd7fb3c9d",
-"flutter_bootstrap.js": "986b4e3d71504f14738e708c4ac22e7b"};
+"flutter_bootstrap.js": "fca4799ab9a8d65d07f44a5f3dd10111"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
