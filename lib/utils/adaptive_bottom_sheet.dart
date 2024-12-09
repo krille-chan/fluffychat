365c49c289a1e9760afd365b59d8519ad6bc24a8
Christian Kußowski <c.kussowski@famedly.com>
2024-12-09 10:29:01 +0100

    chore: Follow up desktop bottom sheet
    Signed-off-by: Krille <c.kussowski@famedly.com>

--- lib/utils/adaptive_bottom_sheet.dart
+++ lib/utils/adaptive_bottom_sheet.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/config/themes.dart';
+import 'package:flutter/material.dart';
 
 Future<T?> showAdaptiveBottomSheet<T>({
   required BuildContext context,
@@ -15,8 +14,9 @@ Future<T?> showAdaptiveBottomSheet<T>({
   return showModalBottomSheet(
     context: context,
     builder: (context) => Padding(
-      padding:
-          dialogMode ? const EdgeInsets.only(bottom: 32.0) : EdgeInsets.zero,
+      padding: dialogMode
+          ? const EdgeInsets.symmetric(vertical: 32.0)
+          : EdgeInsets.zero,
       child: ClipRRect(
         borderRadius: dialogMode
             ? BorderRadius.circular(AppConfig.borderRadius)
@@ -31,8 +31,8 @@ Future<T?> showAdaptiveBottomSheet<T>({
     isDismissible: isDismissible,
     isScrollControlled: isScrollControlled,
     constraints: BoxConstraints(
-      maxHeight: maxHeight + (dialogMode ? 32 : 0),
-      maxWidth: FluffyThemes.columnWidth * 1.25 + 64,
+      maxHeight: maxHeight + (dialogMode ? 64 : 0),
+      maxWidth: FluffyThemes.columnWidth * 1.25,
     ),
     backgroundColor: Colors.transparent,
     showDragHandle: !dialogMode,
