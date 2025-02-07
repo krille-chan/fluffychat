4e36d6010f6f70d47e2961cc22fd40826f347081
krille-chan <krille-chan@users.noreply.github.com>
2025-02-07 09:34:28 +0000

    deploy: 0b48f377749b87b2ee7399ecef33148244873663

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a4574b","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3a5","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1082405035"
+    serviceWorkerVersion: "3063863126"
   }
 });
