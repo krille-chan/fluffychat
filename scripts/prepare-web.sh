0cb45bd9f259a8b2e981b5ff79656313803b7b85
Matthew Scheffel <matt@dataheck.com>
2025-02-15 15:59:45 -0400

    fix: prepare-web script remove quotes from version, but also handle absent directories. remove directory traversal, use cli options

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,10 +1,21 @@
 #!/bin/sh -ve
-rm -r assets/js/package
+ASSETS_DIR=assets/js
+if [ -d "$ASSETS_DIR/package" ]; then
+    rm -r "$ASSETS_DIR/package"
+fi
 
-OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm)
-DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
+OLM_VERSION=$(cat pubspec.yaml | yq -r .dependencies.flutter_olm )
+DOWNLOAD_URL="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
+DOWNLOAD_PATH="/tmp/fluffychat-build-olm.zip"
 
-cd assets/js/ && curl -L $DOWNLOAD_PATH > olm.zip && cd ../../
-cd assets/js/ && unzip olm.zip && cd ../../
-cd assets/js/ && rm olm.zip && cd ../../
-cd assets/js/ && mv javascript package && cd ../../
+mkdir -p "$ASSETS_DIR"
+echo Fetching $DOWNLOAD_URL
+curl -L "$DOWNLOAD_URL" -o "$DOWNLOAD_PATH"
+
+if [ $? = 0 ]; then
+    unzip "$DOWNLOAD_PATH" -d "$ASSETS_DIR"
+    rm "$DOWNLOAD_PATH" 
+    mv "$ASSETS_DIR/javascript" "$ASSETS_DIR/package"
+else
+    echo Failed to download release archive
+fi
