efc81203f042f8c5e9d5dc8b8a85a0f03cd295ab
ggurdin <46800240+ggurdin@users.noreply.github.com>
2025-03-12 11:07:02 -0400

    chore: replace empty page in settings with activity selection page (#2127)

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -41,7 +41,6 @@ import 'package:fluffychat/pangea/spaces/utils/join_with_alias.dart';
 import 'package:fluffychat/pangea/spaces/utils/join_with_link.dart';
 import 'package:fluffychat/pangea/subscription/pages/settings_subscription.dart';
 import 'package:fluffychat/pangea/user/pages/find_partner.dart';
-import 'package:fluffychat/widgets/layouts/empty_page.dart';
 import 'package:fluffychat/widgets/layouts/two_column_layout.dart';
 import 'package:fluffychat/widgets/log_view.dart';
 import 'package:fluffychat/widgets/matrix.dart';
@@ -352,7 +351,10 @@ abstract class AppRoutes {
                     context,
                     state,
                     FluffyThemes.isColumnMode(context)
-                        ? const EmptyPage()
+                        // #Pangea
+                        // ? const EmptyPage()
+                        ? const ActivitySuggestionsArea()
+                        // Pangea#
                         : const Settings(),
                   ),
                   routes: [
