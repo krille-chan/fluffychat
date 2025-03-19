e93fdebe2017f6c6491ceb93d57c71148d1b4f0d
Krille <c.kussowski@famedly.com>
2025-03-19 14:25:37 +0100

    build: Update to flutter 3.29.2
    Signed-off-by: Krille <c.kussowski@famedly.com>

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,36 +3,37 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  app_links
-  desktop_drop
-  dynamic_color
-  emoji_picker_flutter
-  file_selector_windows
-  flutter_secure_storage_windows
-  flutter_webrtc
-  geolocator_windows
-  pasteboard
-  permission_handler_windows
-  record_windows
-  share_plus
-  sqlcipher_flutter_libs
-  url_launcher_windows
-  window_to_front
+        app_links
+        desktop_drop
+        dynamic_color
+        emoji_picker_flutter
+        file_selector_windows
+        flutter_secure_storage_windows
+        flutter_webrtc
+        geolocator_windows
+        pasteboard
+        permission_handler_windows
+        record_windows
+        share_plus
+        sqlcipher_flutter_libs
+        url_launcher_windows
+        window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+        flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
-foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
-  target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
-endforeach(plugin)
+foreach (plugin ${FLUTTER_PLUGIN_LIST})
+    add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
+    target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
+    list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
+    list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
+endforeach (plugin)
 
-foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
-endforeach(ffi_plugin)
+foreach (ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
+    add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
+    list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
+endforeach (ffi_plugin)
