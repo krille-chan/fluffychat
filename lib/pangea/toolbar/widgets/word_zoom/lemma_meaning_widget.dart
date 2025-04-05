af1cc0b8c26ff7d849351e7614da226240a2542c
wcjord <32568597+wcjord@users.noreply.github.com>
2025-04-05 12:54:05 -0400

    fix(lemma_meaning_widget): fix text alignment

--- lib/pangea/toolbar/widgets/word_zoom/lemma_meaning_widget.dart
+++ lib/pangea/toolbar/widgets/word_zoom/lemma_meaning_widget.dart
@@ -1,11 +1,5 @@
 import 'dart:developer';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-import 'package:flutter_gen/gen_l10n/l10n.dart';
-import 'package:material_symbols_icons/symbols.dart';
-
 import 'package:fluffychat/pangea/analytics_misc/construct_use_model.dart';
 import 'package:fluffychat/pangea/analytics_misc/text_loading_shimmer.dart';
 import 'package:fluffychat/pangea/events/models/pangea_token_model.dart';
@@ -18,6 +12,10 @@ import 'package:fluffychat/pangea/toolbar/enums/message_mode_enum.dart';
 import 'package:fluffychat/pangea/toolbar/widgets/message_selection_overlay.dart';
 import 'package:fluffychat/pangea/toolbar/widgets/practice_activity/word_zoom_activity_button.dart';
 import 'package:fluffychat/widgets/matrix.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class LemmaMeaningWidget extends StatefulWidget {
   final ConstructUses constructUse;
@@ -221,6 +219,8 @@ class LemmaMeaningWidgetState extends State<LemmaMeaningWidget> {
               onLongPress: () => _toggleEditMode(true),
               onDoubleTap: () => _toggleEditMode(true),
               child: RichText(
+                textAlign:
+                    widget.leading == null ? TextAlign.center : TextAlign.start,
                 text: TextSpan(
                   style: widget.style?.copyWith(
                     color: widget.controller?.toolbarMode ==
