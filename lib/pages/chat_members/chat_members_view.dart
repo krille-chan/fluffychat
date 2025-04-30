04fd5f3dd90d5a4644d4cf7595ad560365709988
Krille <c.kussowski@famedly.com>
2025-04-30 14:53:16 +0200

    build: Update unifiedpush to 6.0.1
    Signed-off-by: Krille <c.kussowski@famedly.com>

--- lib/pages/chat_members/chat_members_view.dart
+++ lib/pages/chat_members/chat_members_view.dart
@@ -1,9 +1,10 @@
-import 'package:fluffychat/utils/localized_exception_extension.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
 
+import 'package:fluffychat/utils/localized_exception_extension.dart';
 import '../../widgets/layouts/max_width_body.dart';
 import '../../widgets/matrix.dart';
 import '../chat_details/participant_list_item.dart';
