f500fb7aa8eb7f15b721661ebf130d219f2cc354
Steven Lageveen <stevenmax888@gmail.com>
2025-05-01 11:33:46 +0200

    Minor cleanup

--- test/archive_test.dart
+++ test/archive_test.dart
@@ -1,10 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Test if the widget can be created', (WidgetTester tester) async {
-    /*await tester.pumpWidget(FluffyChatApp(
-      testWidget: Archive(),
-      testClient: await testClient(loggedIn: true),
-    ));*/
-  });
+  testWidgets(
+      'Test if the widget can be created', (WidgetTester tester) async {});
 }
