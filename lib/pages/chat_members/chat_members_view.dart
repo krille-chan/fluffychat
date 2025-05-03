d90c1e6fd854a7a45226cb5898eec713a4e33895
Krille <c.kussowski@famedly.com>
2025-05-03 07:22:37 +0200

    chore: Only show joined members in members list by default
    Signed-off-by: Krille <c.kussowski@famedly.com>

--- lib/pages/chat_members/chat_members_view.dart
+++ lib/pages/chat_members/chat_members_view.dart
@@ -1,10 +1,9 @@
+import 'package:fluffychat/utils/localized_exception_extension.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
 
-import 'package:fluffychat/utils/localized_exception_extension.dart';
 import '../../widgets/layouts/max_width_body.dart';
 import '../../widgets/matrix.dart';
 import '../chat_details/participant_list_item.dart';
@@ -145,8 +144,15 @@ class ChatMembersView extends StatelessWidget {
                                             L10n.of(context).banned,
                                           Membership.invite =>
                                             L10n.of(context).invited,
-                                          Membership.join =>
-                                            L10n.of(context).all,
+                                          Membership.join => L10n.of(context)
+                                              .countParticipants(room.summary
+                                                      .mJoinedMemberCount ??
+                                                  controller.members
+                                                      ?.where((member) =>
+                                                          member.membership ==
+                                                          Membership.join)
+                                                      .length ??
+                                                  0),
                                           Membership.knock =>
                                             L10n.of(context).knocking,
                                           Membership.leave =>
