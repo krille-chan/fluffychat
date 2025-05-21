611a8c82401bfd034ffca7165f874d1cfdf515d4
krille-chan <krille-chan@users.noreply.github.com>
2025-05-21 06:24:38 +0000

    deploy: 38f35b7a10a9b3542250cb369c70b7bfa62e4a6e

--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -1,13 +1,13 @@
 ((a,b,c)=>{a[b]=a[b]||{}
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_2",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aC:function aC(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aE:function aE(){}}
 B=c[0]
 A=a.updateHolder(c[54],A)
-A.aC.prototype={
-C(d,e){return this.gba(this).$1(e)}}
+A.aE.prototype={
+B(d,e){return this.gba(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.aC,B.D)})()};
+y(A.aE,B.F)})()};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_2",e:"endPart",h:b})})($__dart_deferred_initializers__,"m7N0zzEmke+bsx+65cMfbgMRfSE=");
+a.eventLog.push({p:"main.dart.js_2",e:"endPart",h:b})})($__dart_deferred_initializers__,"ohbYKLhuHyVqiUZt4+BBb14VThU=");
 //# sourceMappingURL=main.dart.js_2.part.js.map
