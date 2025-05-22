40cceb2176b4a1b1e3071252942a547997f31b06
Christian Kußowski <c.kussowski@famedly.com>
2025-05-22 15:51:39 +0200

    chore: Simplify voiceMessage getter

--- lib/utils/matrix_sdk_extensions/matrix_locals.dart
+++ lib/utils/matrix_sdk_extensions/matrix_locals.dart
@@ -354,19 +354,11 @@ class MatrixLocals extends MatrixLocalizations {
   String get cancelledSend => l10n.sendCanceled;
 
   @override
-  String voiceMessage(String senderName, Duration? duration) {
-    final dateTime = duration == null
-        ? null
-        : DateTime.fromMillisecondsSinceEpoch(
-            duration.inSeconds * 1000,
-          );
-    final formattedDuration = dateTime == null
-        ? ''
-        : DateFormat(
-            DateFormat.MINUTE_SECOND,
-            l10n.localeName,
-          ).format(dateTime);
-
-    return l10n.sentVoiceMessage(senderName, formattedDuration);
-  }
+  String voiceMessage(String senderName, Duration? duration) =>
+      l10n.sentVoiceMessage(
+        senderName,
+        duration == null
+            ? ''
+            : '${duration.inMinutes}:${duration.inSeconds % 60}',
+      );
 }
