5fb882955c6cf9a63eaa325aafa03904b631960f
Christian Kußowski <c.kussowski@famedly.com>
2025-06-01 11:31:09 +0200

    chore: Follow up create check list UX

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1186,15 +1186,6 @@ class ChatController extends State<ChatPageWithRoom>
     if (choice == 'location') {
       sendLocationAction();
     }
-    if (choice == 'checklist') {
-      if (sendController.text.isEmpty) {
-        sendController.text = '- [ ] ';
-      } else {
-        sendController.text += '\n- [ ] ';
-      }
-      onInputBarChanged(sendController.text);
-      inputFocus.requestFocus();
-    }
   }
 
   unpinEvent(String eventId) async {
