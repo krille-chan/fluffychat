7be5099cb05f4d1ea504c1a81e27636b2a804866
ggurdin <ggurdin@gmail.com>
2025-06-05 09:52:05 -0400

    chore: scroll up on page after selecting search bar

--- lib/pangea/activity_suggestions/suggestions_page.dart
+++ lib/pangea/activity_suggestions/suggestions_page.dart
@@ -15,8 +15,9 @@ class SuggestionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isColumnMode = FluffyThemes.isColumnMode(context);
-    return Material(
-      child: SafeArea(
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
