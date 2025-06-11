d8f61cd7e36407eabbdc65ac7e317751d34e9d04
ggurdin <ggurdin@gmail.com>
2025-06-11 15:26:50 -0400

    fix localization imports

--- lib/pangea/public_spaces/public_room_bottom_sheet.dart
+++ lib/pangea/public_spaces/public_room_bottom_sheet.dart
@@ -3,11 +3,11 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:matrix/matrix.dart';
 
+import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/pangea/common/config/environment.dart';
 import 'package:fluffychat/pangea/spaces/constants/space_constants.dart';
 import 'package:fluffychat/utils/adaptive_bottom_sheet.dart';
