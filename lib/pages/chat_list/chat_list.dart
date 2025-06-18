ecac8fd48f07fd1b09142eb26806803a163fb5d3
Kelrap <kel.raphael3@outlook.com>
2025-06-18 11:04:33 -0400

    Limit size of chat selection popup title

--- lib/pages/chat_list/chat_list.dart
+++ lib/pages/chat_list/chat_list.dart
@@ -46,6 +46,7 @@ import '../../widgets/matrix.dart';
 import 'package:fluffychat/utils/tor_stub.dart'
     if (dart.library.html) 'package:tor_detector_web/tor_detector_web.dart';
 
+
 enum PopupMenuAction {
   settings,
   invite,
@@ -753,10 +754,19 @@ class ChatListController extends State<ChatList>
                 // Pangea#
               ),
               const SizedBox(width: 12),
-              Text(
-                displayname,
-                style:
-                    TextStyle(color: Theme.of(context).colorScheme.onSurface),
+              // #Pangea
+              Flexible(
+                child:
+                    // Pangea#
+                    Text(
+                  displayname,
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.onSurface),
+                  // #Pangea
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  // Pangea#
+                ),
               ),
             ],
           ),
