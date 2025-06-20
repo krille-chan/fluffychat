a396b94becf8ddfe46649eaa343a3de42f08ddf5
wcjord <32568597+wcjord@users.noreply.github.com>
2025-06-20 09:18:17 -0400

    fix(phonetic_transcription): show transcriptions if scripts null in languages

--- lib/pangea/learning_settings/controllers/language_controller.dart
+++ lib/pangea/learning_settings/controllers/language_controller.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:flutter/material.dart';
-
-import 'package:universal_io/io.dart';
-
 import 'package:fluffychat/pangea/common/controllers/pangea_controller.dart';
 import 'package:fluffychat/pangea/learning_settings/constants/language_constants.dart';
 import 'package:fluffychat/pangea/learning_settings/models/language_model.dart';
 import 'package:fluffychat/pangea/learning_settings/utils/p_language_store.dart';
+import 'package:flutter/material.dart';
+import 'package:universal_io/io.dart';
+
 import '../widgets/p_language_dialog.dart';
 
 class LanguageController {
@@ -114,8 +113,19 @@ class LanguageController {
   }
 
   bool get showTrancription =>
+<<<<<<< Updated upstream
       _pangeaController.languageController.userL1 != null &&
       _pangeaController.languageController.userL2 != null &&
       _pangeaController.languageController.userL1?.script !=
           _pangeaController.languageController.userL2?.script;
+=======
+      (_pangeaController.languageController.userL1 != null &&
+          _pangeaController.languageController.userL2 != null &&
+          _pangeaController.languageController.userL1?.script !=
+              _pangeaController.languageController.userL2?.script) ||
+      (_pangeaController.languageController.userL1?.script !=
+              LanguageKeys.unknownLanguage ||
+          _pangeaController.languageController.userL2?.script ==
+              LanguageKeys.unknownLanguage);
+>>>>>>> Stashed changes
 }
--- lib/pangea/learning_settings/models/language_model.dart
+++ lib/pangea/learning_settings/models/language_model.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
-
 import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/pangea/learning_settings/constants/language_constants.dart';
 import 'package:fluffychat/pangea/learning_settings/enums/l2_support_enum.dart';
+import 'package:flutter/material.dart';
 
 class LanguageModel {
   final String langCode;
