5d8bad7458f671ef82003c8eb05596e71f35a501
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-06-21 01:32:50 +0900

    chore: do format

--- lib/widgets/local_notifications_extension.dart
+++ lib/widgets/local_notifications_extension.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
-import 'package:collection/collection.dart';
-import 'package:fluffychat/widgets/fluffy_chat_app.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:collection/collection.dart';
 import 'package:desktop_notifications/desktop_notifications.dart';
 import 'package:matrix/matrix.dart';
 import 'package:universal_html/html.dart' as html;
@@ -14,6 +13,7 @@ import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/utils/client_download_content_extension.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
+import 'package:fluffychat/widgets/fluffy_chat_app.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 
 extension LocalNotificationsExtension on MatrixState {
