4571349eab08e0912c8e5f44c3ecebca54930bab
ggurdin <ggurdin@gmail.com>
2025-06-23 10:59:39 -0400

    refactor: word card redesign

--- lib/pangea/toolbar/widgets/word_zoom/word_zoom_widget.dart
+++ lib/pangea/toolbar/widgets/word_zoom/word_zoom_widget.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'package:fluffychat/config/app_config.dart';
+import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/pangea/analytics_details_popup/analytics_details_popup.dart';
 import 'package:fluffychat/pangea/analytics_misc/construct_type_enum.dart';
 import 'package:fluffychat/pangea/events/event_wrappers/pangea_message_event.dart';
 import 'package:fluffychat/pangea/events/models/pangea_token_model.dart';
-import 'package:fluffychat/pangea/learning_settings/constants/language_constants.dart';
+import 'package:fluffychat/pangea/learning_settings/models/language_model.dart';
+import 'package:fluffychat/pangea/learning_settings/utils/p_language_store.dart';
 import 'package:fluffychat/pangea/lemmas/construct_xp_widget.dart';
 import 'package:fluffychat/pangea/lemmas/lemma_reaction_picker.dart';
+import 'package:fluffychat/pangea/phonetic_transcription/phonetic_transcription_widget.dart';
 import 'package:fluffychat/pangea/practice_activities/activity_type_enum.dart';
 import 'package:fluffychat/pangea/toolbar/widgets/message_selection_overlay.dart';
-import 'package:fluffychat/pangea/toolbar/widgets/word_zoom/lemma_meaning_widget.dart';
-import 'package:fluffychat/pangea/toolbar/widgets/word_zoom/lemma_widget.dart';
+import 'package:fluffychat/pangea/toolbar/widgets/practice_activity/word_audio_button.dart';
+import 'package:fluffychat/pangea/toolbar/widgets/word_zoom/lemma_meaning_builder.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 
 class WordZoomWidget extends StatelessWidget {
@@ -28,8 +31,6 @@ class WordZoomWidget extends StatelessWidget {
 
   PangeaToken get _selectedToken => overlayController.selectedToken!;
 
-  void _onEditDone() => overlayController.initializeTokensAndMode();
-
   bool get hasEmojiActivity =>
       overlayController.practiceSelection?.hasActiveActivityByToken(
             ActivityTypeEnum.emoji,
@@ -40,159 +41,199 @@ class WordZoomWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
+    return Container(
+      padding: const EdgeInsets.all(12.0),
       constraints: const BoxConstraints(
-        minHeight: AppConfig.toolbarMinHeight,
-        maxHeight: AppConfig.toolbarMaxHeight,
+        minHeight: AppConfig.toolbarMinHeight - 8,
+        maxHeight: AppConfig.toolbarMaxHeight - 8,
         maxWidth: AppConfig.toolbarMinWidth,
       ),
       child: SingleChildScrollView(
-        padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          spacing: 12.0,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Container(
-              constraints: const BoxConstraints(
-                minHeight: 40,
-              ),
-              color: Theme.of(context).colorScheme.surface,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  //@ggurdin - might need to play with size to properly center
-                  SizedBox(
-                    width: 24.0,
-                    height: 24.0,
-                    child: IconButton(
-                      onPressed: () => overlayController.updateSelectedSpan(
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 24.0,
+                  height: 24.0,
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      onTap: () => overlayController.updateSelectedSpan(
                         token.text,
                       ),
-                      icon: const Icon(Icons.close),
-                      style: IconButton.styleFrom(
-                        padding: EdgeInsets.zero,
+                      child: const Icon(
+                        Icons.close,
+                        size: 16.0,
                       ),
                     ),
                   ),
-                  LemmaWidget(
-                    token: _selectedToken,
-                    pangeaMessageEvent: messageEvent,
-                    // onEdit: () => _setHideCenterContent(true),
-                    onEdit: () {
-                      debugPrint("what are we doing edits with?");
-                    },
-                    onEditDone: () {
-                      debugPrint("what are we doing edits with?");
-                      _onEditDone();
-                    },
-                    overlayController: overlayController,
+                ),
+                Text(
+                  token.text.content,
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w600,
+                    height: 1.2,
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? AppConfig.yellowDark
+                        : AppConfig.yellowLight,
                   ),
-                  ConstructXpWidget(
-                    id: token.vocabConstructID,
-                    onTap: () => showDialog<AnalyticsPopupWrapper>(
-                      context: context,
-                      builder: (context) => AnalyticsPopupWrapper(
-                        constructZoom: token.vocabConstructID,
-                        view: ConstructTypeEnum.vocab,
-                      ),
+                ),
+                ConstructXpWidget(
+                  id: token.vocabConstructID,
+                  onTap: () => showDialog<AnalyticsPopupWrapper>(
+                    context: context,
+                    builder: (context) => AnalyticsPopupWrapper(
+                      constructZoom: token.vocabConstructID,
+                      view: ConstructTypeEnum.vocab,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 8.0,
+            LemmaMeaningBuilder(
+              langCode: messageEvent.messageDisplayLangCode,
+              constructId: token.vocabConstructID,
+              builder: (context, controller) {
+                if (controller.editMode) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "${L10n.of(context).pangeaBotIsFallible} ${L10n.of(context).whatIsMeaning(
+                          token.vocabConstructID.lemma,
+                          token.vocabConstructID.category,
+                        )}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: TextField(
+                          minLines: 1,
+                          maxLines: 3,
+                          controller: controller.controller,
+                          decoration: InputDecoration(
+                            hintText: controller.lemmaInfo?.meaning,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () => controller.toggleEditMode(false),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                            child: Text(L10n.of(context).cancel),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () => controller.controller.text !=
+                                        controller.lemmaInfo?.meaning &&
+                                    controller.controller.text.isNotEmpty
+                                ? controller.editLemmaMeaning(
+                                    controller.controller.text,
+                                  )
+                                : null,
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                            child: Text(L10n.of(context).saveChanges),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                }
+
+                return Column(
+                  spacing: 12.0,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (MatrixState
+                        .pangeaController.languageController.showTrancription)
+                      PhoneticTranscriptionWidget(
+                        text: token.text.content,
+                        textLanguage: PLanguageStore.byLangCode(
+                              messageEvent.messageDisplayLangCode,
+                            ) ??
+                            LanguageModel.unknown,
+                        style: const TextStyle(fontSize: 14.0),
+                        iconSize: 24.0,
+                      )
+                    else
+                      WordAudioButton(
+                        text: token.text.content,
+                        uniqueID: "lemma-content-${token.text.content}",
+                        langCode: messageEvent.messageDisplayLangCode,
+                        iconSize: 24.0,
+                      ),
+                    LemmaReactionPicker(
+                      cId: _selectedToken.vocabConstructID,
+                      controller: overlayController.widget.chatController,
+                    ),
+                    if (controller.error != null)
+                      Text(
+                        L10n.of(context).oopsSomethingWentWrong,
+                        textAlign: TextAlign.center,
+                      )
+                    else if (controller.isLoading ||
+                        controller.lemmaInfo == null)
+                      const CircularProgressIndicator.adaptive()
+                    else
+                      GestureDetector(
+                        onLongPress: () => controller.toggleEditMode(true),
+                        onDoubleTap: () => controller.toggleEditMode(true),
+                        child: token.lemma.text == token.text.content
+                            ? Text(
+                                controller.lemmaInfo!.meaning,
+                                style: const TextStyle(fontSize: 14.0),
+                                textAlign: TextAlign.center,
+                              )
+                            : RichText(
+                                text: TextSpan(
+                                  style: DefaultTextStyle.of(context)
+                                      .style
+                                      .copyWith(
+                                        fontSize: 14.0,
+                                      ),
+                                  children: [
+                                    TextSpan(text: token.lemma.text),
+                                    const WidgetSpan(
+                                      child: SizedBox(width: 8.0),
+                                    ),
+                                    const TextSpan(text: ":"),
+                                    const WidgetSpan(
+                                      child: SizedBox(width: 8.0),
+                                    ),
+                                    TextSpan(
+                                      text: controller.lemmaInfo!.meaning,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                      ),
+                  ],
+                );
+              },
             ),
-            LemmaReactionPicker(
-              cId: _selectedToken.vocabConstructID,
-              controller: overlayController.widget.chatController,
-            ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Container(
-            //       constraints: const BoxConstraints(
-            //         minHeight: 40,
-            //       ),
-            //       alignment: Alignment.center,
-            // child: LemmaEmojiRow(
-            //   cId: _selectedToken.vocabConstructID,
-            //   onTapOverride: overlayController.hideWordCardContent &&
-            //           hasEmojiActivity
-            //       ? () => overlayController.updateToolbarMode(
-            //             MessageMode.wordEmoji,
-            //           )
-            //       : null,
-            //   isSelected:
-            //       overlayController.toolbarMode == MessageMode.wordEmoji,
-            //   emojiSetCallback: () => overlayController.setState(() {}),
-            //   shouldShowEmojis: !hasEmojiActivity,
-            // ),
-            //     ),
-            //   ],
-            // ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            Container(
-              constraints: const BoxConstraints(
-                minHeight: 40,
-              ),
-              alignment: Alignment.center,
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                runAlignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 8,
-                children: [
-                  LemmaMeaningWidget(
-                    constructUse: token.vocabConstructID.constructUses,
-                    langCode: MatrixState.pangeaController.languageController
-                            .userL2?.langCodeShort ??
-                        LanguageKeys.defaultLanguage,
-                    token: overlayController.selectedToken!,
-                    controller: overlayController,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ],
-              ),
-            ),
-            // const SizedBox(
-            //   height: 8.0,
-            // ),
-            // Wrap(
-            //   alignment: WrapAlignment.center,
-            //   crossAxisAlignment: WrapCrossAlignment.center,
-            //   spacing: 8.0,
-            //   children: [
-            //     if (token.text.content.toLowerCase() !=
-            //         token.lemma.text.toLowerCase()) ...[
-            //       Text(
-            //         _selectedToken.text.content,
-            //         style: Theme.of(context).textTheme.bodyLarge,
-            //         overflow: TextOverflow.ellipsis,
-            //       ),
-            //       WordAudioButton(
-            //         text: _selectedToken.text.content,
-            //         baseOpacity: 0.4,
-            //         uniqueID: "word-zoom-audio-${_selectedToken.text.content}",
-            //         langCode: overlayController
-            //             .pangeaMessageEvent!.messageDisplayLangCode,
-            //       ),
-            //     ],
-            //     ..._selectedToken.morphsBasicallyEligibleForPracticeByPriority
-            //         .map(
-            //       (cId) => MorphologicalListItem(
-            //         morphFeature: MorphFeaturesEnumExtension.fromString(
-            //           cId.category,
-            //         ),
-            //         token: _selectedToken,
-            //         overlayController: overlayController,
-            //       ),
-            //     ),
-            //   ],
-            // ),
           ],
         ),
       ),
