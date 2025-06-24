36141d4043f149c72187f81ae092ac617bd8e767
ggurdin <ggurdin@gmail.com>
2025-06-24 09:13:42 -0400

    chore: formatting

--- lib/pangea/choreographer/widgets/it_bar.dart
+++ lib/pangea/choreographer/widgets/it_bar.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
 import 'package:fluffychat/pangea/choreographer/constants/choreo_constants.dart';
 import 'package:fluffychat/pangea/choreographer/controllers/choreographer.dart';
 import 'package:fluffychat/pangea/choreographer/controllers/it_controller.dart';
@@ -11,9 +14,6 @@ import 'package:fluffychat/pangea/instructions/instructions_enum.dart';
 import 'package:fluffychat/pangea/instructions/instructions_inline_tooltip.dart';
 import 'package:fluffychat/pangea/learning_settings/pages/settings_learning.dart';
 import 'package:fluffychat/widgets/matrix.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
 import '../../common/utils/overlay.dart';
 import '../controllers/it_feedback_controller.dart';
 import '../models/it_response_model.dart';
@@ -358,7 +358,7 @@ class ITChoices extends StatelessWidget {
                 sourceTextLang: controller.sourceLangCode,
                 targetLang: controller.targetLangCode,
               ),
-              choiceFeedback: choiceFeedback ?? "",
+              choiceFeedback: choiceFeedback,
             ),
       maxHeight: 300,
       maxWidth: 300,
