c545ddf02c10d14579157a284977e0b3ddf84a44
ggurdin <ggurdin@gmail.com>
2025-06-25 17:30:06 -0400

    chore: on click space in nav rail, if invited, show invite popup

--- lib/pages/chat_list/chat_list.dart
+++ lib/pages/chat_list/chat_list.dart
@@ -587,7 +587,6 @@ class ChatListController extends State<ChatList>
           if (space != null) {
             chatListHandleSpaceTap(
               context,
-              this,
               space,
             );
           }
