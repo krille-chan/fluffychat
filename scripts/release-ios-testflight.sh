4a9efddf15508eb20c2bdb293b17b67e30b6a939
Christian Kußowski <c.kussowski@famedly.com>
2025-07-12 10:44:52 +0200

    build: Update release ios script

--- scripts/release-ios-testflight.sh
+++ scripts/release-ios-testflight.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ve
-./scripts/add-firebase-messaging.sh
+flutter pub add fcm_shared_isolate:0.1.0
+sed -i '' 's,//<GOOGLE_SERVICES>,,g' lib/utils/background_push.dart
 yq eval '.dependencies.fcm_shared_isolate = "0.1.0"' -i pubspec.yaml # Workaround: 0.2.0 does not work on iOS
 flutter clean
 flutter pub get
