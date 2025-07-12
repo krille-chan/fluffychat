f798421dd2885fa856dbcf0017c13d2db75f62a7
Christian Kußowski <c.kussowski@famedly.com>
2025-07-12 11:28:43 +0200

    build: Add missing incomment fcm push service

--- scripts/add-firebase-messaging.sh
+++ scripts/add-firebase-messaging.sh
@@ -5,6 +5,8 @@ flutter pub get
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's,//<GOOGLE_SERVICES>,,g' lib/utils/background_push.dart
+  sed -i '' -e 's,^/\*,,' -e 's,\*/$,,' android/app/src/main/kotlin/chat/fluffy/fluffychat/FcmPushService.kt
 else
   sed -i 's,//<GOOGLE_SERVICES>,,g' lib/utils/background_push.dart
+  sed -i -e 's,^/\*,,' -e 's,\*/$,,' android/app/src/main/kotlin/chat/fluffy/fluffychat/FcmPushService.kt
 fi
