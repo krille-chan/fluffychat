4e8e9eb6afb7601145903d58c8401a9b40e023c6
ggurdin <46800240+ggurdin@users.noreply.github.com>
2025-07-23 11:09:26 -0400

    chore: remove unused choreo URLs (#3540)

--- lib/pangea/common/network/urls.dart
+++ lib/pangea/common/network/urls.dart
@@ -29,13 +29,6 @@ class PApiUrls {
   ///   ---------------------- Users --------------------------------------
   static String paymentLink = "${PApiUrls.subscriptionEndpoint}/payment_link";
 
-  ///   ---------------------- Conversation Partner -------------------------
-  /// PTODO: Migrate or remove
-  static String searchUserProfiles = "${PApiUrls.accountEndpoint}/search";
-
-  ///-------------------------------- choreo --------------------------
-  static String igc = "${PApiUrls.choreoEndpoint}/grammar";
-
   static String languageDetection =
       "${PApiUrls.choreoEndpoint}/language_detection";
 
@@ -43,20 +36,16 @@ class PApiUrls {
   static String spanDetails = "${PApiUrls.choreoEndpoint}/span_details";
 
   static String wordNet = "${PApiUrls.choreoEndpoint}/wordnet";
-  static String contextualizedTranslation =
-      "${PApiUrls.choreoEndpoint}/translation/contextual";
   static String simpleTranslation =
       "${PApiUrls.choreoEndpoint}/translation/direct";
   static String tokenize = "${PApiUrls.choreoEndpoint}/tokenize";
   static String contextualDefinition =
       "${PApiUrls.choreoEndpoint}/contextual_definition";
   static String similarity = "${PApiUrls.choreoEndpoint}/similarity";
-  static String topicInfo = "${PApiUrls.choreoEndpoint}/vocab_list";
 
   static String itFeedback = "${PApiUrls.choreoEndpoint}/translation/feedback";
 
   static String firstStep = "${PApiUrls.choreoEndpoint}/it_initialstep";
-  static String subseqStep = "${PApiUrls.choreoEndpoint}/it_step";
 
   static String textToSpeech = "${PApiUrls.choreoEndpoint}/text_to_speech";
   static String speechToText = "${PApiUrls.choreoEndpoint}/speech_to_text";
