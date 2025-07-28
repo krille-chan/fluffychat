81348e78297207fdf6412f9fbb9185832bb8d10e
Ana Elisa Ramos <anaelisa.mramos@gmail.com>
2025-07-28 15:34:46 -0300

    💄 Positioning audio player on list view

--- lib/pages/chat_list/chat_list_view.dart
+++ lib/pages/chat_list/chat_list_view.dart
@@ -9,7 +9,7 @@ import 'package:fluffychat/pages/chat_list/chat_list.dart';
 import 'package:fluffychat/widgets/navigation_rail.dart';
 import 'chat_list_body.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:fluffychat/widgets/audio_player_stream.dart';
+import 'package:fluffychat/widgets/streaming/audio_player_streaming.dart';
 
 class ChatListView extends StatelessWidget {
   final ChatListController controller;
@@ -55,56 +55,32 @@ class ChatListView extends StatelessWidget {
                   children: [
                     ChatListViewBody(controller),
                     Positioned(
-                      bottom: MediaQuery.of(context).size.height *
-                          0.2, // mesma altura do banner
-                      left: 16,
-                      right: 16,
-                      child: Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.surface,
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 6,
-                              offset: Offset(0, -2),
-                            ),
-                          ],
-                        ),
-                        child: const AudioPlayerStream(),
-                      ),
-                    ),
-                    Positioned(
                       bottom: 0,
                       left: 0,
                       right: 0,
-                      child: Container(
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        color: Theme.of(context).colorScheme.surface,
-                        child: Padding(
-                          padding: EdgeInsets.all(
-                            MediaQuery.of(context).size.width * 0.01,
-                          ),
-                          child: MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: GestureDetector(
-                              onTap: () async {
-                                final uri = Uri.parse(
-                                    'https://www.radiohemp.com/chama/');
-                                if (await canLaunchUrl(uri)) {
-                                  await launchUrl(uri,
-                                      mode: LaunchMode.externalApplication);
-                                }
-                              },
-                              child: Image.asset(
-                                'assets/banner-de-apoio-CHAMA.png',
-                                fit: BoxFit.contain,
-                                filterQuality: FilterQuality.high,
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          return Center(
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(maxWidth: 500),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.only(
+                                      left: 20,
+                                      right: 20,
+                                      bottom: 16,
+                                      top: 20,
+                                    ),
+                                    child: const AudioPlayerStreaming(),
+                                  ),
+                                ],
                               ),
                             ),
-                          ),
-                        ),
+                          );
+                        },
                       ),
                     ),
                   ],
