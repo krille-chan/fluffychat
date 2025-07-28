6034dceac8dd3643737d160728833024aaee392d
krille-chan <krille-chan@users.noreply.github.com>
2025-07-28 08:13:59 +0000

    deploy: fd0651c3655c4eeb7a52eceeb37d545ef735c563

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524b2","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a5767","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1453002091"
+    serviceWorkerVersion: "3266770853"
   }
 });
