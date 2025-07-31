7fb42cc9805d188f21fec0aa0a85bbf735b574ba
avashilling <165050625+avashilling@users.noreply.github.com>
2025-07-31 13:51:47 -0400

    fix: limit redaction message (#3600)
    * fix: limit redaction message
    
    Doesn't do much since redaction messages aren't currently shown, but will limit them to a reasonable length if they're ever added back
    
    * add pangea tags

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1354,6 +1354,9 @@ class ChatController extends State<ChatPageWithRoom>
             message: L10n.of(context).redactMessageDescription,
             isDestructive: true,
             hintText: L10n.of(context).optionalRedactReason,
+            // #Pangea
+            maxLength: 255,
+            // Pangea#
             okLabel: L10n.of(context).remove,
             cancelLabel: L10n.of(context).cancel,
             // #Pangea
