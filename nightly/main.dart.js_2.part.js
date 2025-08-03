56637368ec33f3c0d1f94f604a28bea3cd642db7
krille-chan <krille-chan@users.noreply.github.com>
2025-08-03 08:29:49 +0000

    deploy: d879c27e3a79b76189d74034e1c87a2b2bf5a43c

--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -7,7 +7,7 @@ A=a.updateHolder(c[54],A)
 A.aF.prototype={
 B(d,e){return this.gba(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.aF,B.C)})()};
+y(A.aF,B.D)})()};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_2",e:"endPart",h:b})})($__dart_deferred_initializers__,"fqozVF6l7eQEExGTI3vl87/NkEo=");
+a.eventLog.push({p:"main.dart.js_2",e:"endPart",h:b})})($__dart_deferred_initializers__,"buBymxoBOXXmrf5JFmGi7tjwv8A=");
 //# sourceMappingURL=main.dart.js_2.part.js.map
