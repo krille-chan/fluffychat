451ced6580afc03a127cc699dfb59c7feed22f5d
Kelrap <99418823+Kelrap@users.noreply.github.com>
2025-08-04 16:39:04 -0400

    Edit unpin confirmation text (#3620)
    * Edit unpin confirmation text
    
    * Add new intl entry for edited unpin confirmation text
    
    * Add Pangea comments
    
    * Fix pangea comments
    
    ---------
    
    Co-authored-by: ggurdin <ggurdin@gmail.com>

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1805,7 +1805,10 @@ class ChatController extends State<ChatPageWithRoom>
     final response = await showOkCancelAlertDialog(
       context: context,
       title: L10n.of(context).unpin,
-      message: L10n.of(context).confirmEventUnpin,
+      // #Pangea
+      // message: L10n.of(context).confirmEventUnpin,
+      message: L10n.of(context).confirmMessageUnpin,
+      // Pangea#
       okLabel: L10n.of(context).unpin,
       cancelLabel: L10n.of(context).cancel,
     );
