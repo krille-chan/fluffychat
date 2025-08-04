66c2c6b9d1125d4131a140a6502495dfc4d7cd80
Kelrap <kel.raphael3@outlook.com>
2025-08-04 11:48:16 -0400

    fix: Edit unpin confirmation text

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1177,7 +1177,7 @@ class ChatController extends State<ChatPageWithRoom>
     final response = await showOkCancelAlertDialog(
       context: context,
       title: L10n.of(context).unpin,
-      message: L10n.of(context).confirmEventUnpin,
+      message: L10n.of(context).confirmMessageUnpin,
       okLabel: L10n.of(context).unpin,
       cancelLabel: L10n.of(context).cancel,
     );
