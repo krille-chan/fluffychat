d1b7b13ab6e075e69abc1041ac0998df820b050f
ggurdin <46800240+ggurdin@users.noreply.github.com>
2025-08-07 12:04:15 -0400

    chore: don't set l1 if locale is not valid (#3654)

--- lib/pangea/learning_settings/utils/locale_provider.dart
+++ lib/pangea/learning_settings/utils/locale_provider.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart' as intl;
+
 class LocaleProvider extends ChangeNotifier {
   Locale? _locale;
 
   Locale? get locale => _locale;
 
   void setLocale(String? value) {
-    if (value == null || value.isEmpty) {
+    final split = value?.split('_');
+    if (split == null ||
+        split.isEmpty ||
+        !intl.DateFormat.localeExists(split[0])) {
       _locale = null;
       notifyListeners();
       return;
     }
 
-    final split = value.split('-');
     _locale = Locale(split[0], split.length > 1 ? split[1] : null);
     notifyListeners();
   }
