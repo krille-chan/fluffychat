76be960913f076f4713842097acd169eb8118032
krille-chan <krille-chan@users.noreply.github.com>
2025-09-02 13:04:38 +0000

    deploy: 34492bd45e74c00740503249373e53563cb0dbbd

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2890579600"
+    serviceWorkerVersion: "1673823735"
   }
 });
--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -3,8 +3,8 @@ $__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aF:function a
 B=c[0]
 A=a.updateHolder(c[54],A)
 A.aF.prototype={
-B(d,e){return this.gba(this).$1(e)}}
+A(d,e){return this.gba(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
 y(A.aF,B.E)})()};
-(a=>{a["bbWPlMF0tLPWN2KBnlsIYcLgVB0="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["2NIwaCVl5ImxPXmaTiRjwiD8K/o="]=a.current})($__dart_deferred_initializers__);
 //# sourceMappingURL=main.dart.js_2.part.js.map
