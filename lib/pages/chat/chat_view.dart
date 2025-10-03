5d4d51b10b1737348bee5303131b3e98d971a1ec
Kelrap <99418823+Kelrap@users.noreply.github.com>
2025-10-03 11:51:22 -0400

    Adjust unread badge height to compensate for activity header height (#4213)
    * Adjust unread badge height to compensate for activity header height
    
    * Limit height instead of hardcoding values

--- lib/pages/chat/chat_view.dart
+++ lib/pages/chat/chat_view.dart
@@ -243,11 +243,21 @@ class ChatView extends StatelessWidget {
                                     .where(
                                       (syncUpdate) => syncUpdate.hasRoomUpdate,
                                     ),
-                                builder: (context, _) => UnreadRoomsBadge(
-                                  filter: (r) => r.id != controller.roomId,
-                                  badgePosition:
-                                      BadgePosition.topEnd(end: 8, top: 4),
-                                  child: const Center(child: BackButton()),
+                                // #Pangea
+                                // builder: (context, _) => UnreadRoomsBadge(
+                                builder: (context, _) => Center(
+                                  child: SizedBox(
+                                    height: kToolbarHeight,
+                                    child: UnreadRoomsBadge(
+                                      // Pangea#
+                                      filter: (r) => r.id != controller.roomId,
+                                      badgePosition: BadgePosition.topEnd(
+                                        end: 8,
+                                        top: 4,
+                                      ),
+                                      child: const Center(child: BackButton()),
+                                    ),
+                                  ),
                                 ),
                               ),
                 titleSpacing: FluffyThemes.isColumnMode(context) ? 24 : 0,
