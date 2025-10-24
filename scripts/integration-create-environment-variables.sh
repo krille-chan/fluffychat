8c8853484fcb6649a03409c0bb05fd697410a433
Christian Kußowski <c.kussowski@famedly.com>
2025-10-24 15:43:41 +0200

    build: Add maestro based integration tests

--- scripts/integration-create-environment-variables.sh
+++ scripts/integration-create-environment-variables.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+set -e
 
-export USER1_NAME="alice"
-export USER1_PW="AliceInWonderland"
-export USER2_NAME="bob"
-export USER2_PW="JoWirSchaffenDas"
+ENV_FILE="${1:-integration_test/data/integration_users.env}"
+
+if [ ! -f "$ENV_FILE" ]; then
+  echo "❌ .env file not found: $ENV_FILE"
+  exit 1
+fi
+
+# read and export user data environment variables from file
+set -o allexport
+source "$ENV_FILE"
+set +o allexport
+
+echo "✅ Environment variables exported from $ENV_FILE"
+
