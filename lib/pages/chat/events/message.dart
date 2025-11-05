4786778449988a0969ac370a403b00a6df7fc8ae
Christian Kußowski <c.kussowski@famedly.com>
2025-11-05 09:41:17 +0100

    chore: Follow up thread design

--- lib/pages/chat/events/message.dart
+++ lib/pages/chat/events/message.dart
@@ -879,7 +879,7 @@ class Message extends StatelessWidget {
                       ? const SizedBox.shrink()
                       : Padding(
                           padding: const EdgeInsets.only(
-                            top: 4.0,
+                            top: 2.0,
                             bottom: 8.0,
                             left: Avatar.defaultSize + 8,
                           ),
