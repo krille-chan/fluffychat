30f31be741601a956fc595426381f4318f196cc9
krille-chan <krille-chan@users.noreply.github.com>
2025-12-03 07:16:32 +0000

    deploy: 037393c5983c9dcbf5654e103b8fafd9317edefd

--- nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
+++ nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
@@ -1528,24 +1528,6 @@ let wasm_bindgen;
         return ret;
     };
 
-    function takeFromExternrefTable0(idx) {
-        const value = wasm.__wbindgen_export_2.get(idx);
-        wasm.__externref_table_dealloc(idx);
-        return value;
-    }
-    /**
-     * @param {number} ptr
-     * @returns {any}
-     */
-    __exports.frb_dart_opaque_rust2dart_decode = function(ptr) {
-        const ret = wasm.frb_dart_opaque_rust2dart_decode(ptr);
-        return ret;
-    };
-
-    __exports.wasm_start_callback = function() {
-        wasm.wasm_start_callback();
-    };
-
     /**
      * @param {number} ptr
      */
@@ -1561,6 +1543,12 @@ let wasm_bindgen;
         }
         WASM_VECTOR_LEN = array.length;
         return ptr;
+    }
+
+    function takeFromExternrefTable0(idx) {
+        const value = wasm.__wbindgen_export_2.get(idx);
+        wasm.__externref_table_dealloc(idx);
+        return value;
     }
     /**
      * ## Safety
@@ -1593,8 +1581,21 @@ let wasm_bindgen;
         return ret >>> 0;
     };
 
+    /**
+     * @param {number} ptr
+     * @returns {any}
+     */
+    __exports.frb_dart_opaque_rust2dart_decode = function(ptr) {
+        const ret = wasm.frb_dart_opaque_rust2dart_decode(ptr);
+        return ret;
+    };
+
+    __exports.wasm_start_callback = function() {
+        wasm.wasm_start_callback();
+    };
+
     function __wbg_adapter_40(arg0, arg1, arg2) {
-        wasm.closure586_externref_shim(arg0, arg1, arg2);
+        wasm.closure598_externref_shim(arg0, arg1, arg2);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -1954,8 +1955,8 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper1737 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 587, __wbg_adapter_40);
+        imports.wbg.__wbindgen_closure_wrapper1736 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 599, __wbg_adapter_40);
             return ret;
         };
         imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1818753928"
+    serviceWorkerVersion: "1303662277"
   }
 });
