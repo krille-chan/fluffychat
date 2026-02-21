f2e87b9f13307c39b892a9f29ce00d89ef8ccacd
krille-chan <krille-chan@users.noreply.github.com>
2026-02-21 06:56:05 +0000

    deploy: b1a2b96aa42b082422ac36527e17002add4dae0b

--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -1,10 +1,10 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aE:function aE(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aD:function aD(){}}
 B=c[0]
-A=a.updateHolder(c[56],A)
-A.aE.prototype={
-A(d,e){return this.gbb(this).$1(e)}}
+A=a.updateHolder(c[57],A)
+A.aD.prototype={
+A(d,e){return this.gb9(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.aE,B.C)})()};
-(a=>{a["RWxxz1pwewXsebd47aIC0rLI69A="]=a.current})($__dart_deferred_initializers__);
+y(A.aD,B.C)})()};
+(a=>{a["G5p/qcMDWjyG8mKqfLGgYdI3hdg="]=a.current})($__dart_deferred_initializers__);
 //# sourceMappingURL=main.dart.js_2.part.js.map
