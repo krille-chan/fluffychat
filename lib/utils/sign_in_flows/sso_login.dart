a7c89418ebc8513a5889a828ca468660c479ef2e
Christian Kußowski <c.kussowski@famedly.com>
2026-03-10 10:04:08 +0100

    chore: Implement pick SSO provider from dialog and add tchncs.de

--- lib/utils/sign_in_flows/sso_login.dart
+++ lib/utils/sign_in_flows/sso_login.dart
@@ -3,20 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 import 'package:matrix/matrix.dart';
 
+import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/utils/platform_infos.dart';
 import 'package:fluffychat/utils/sign_in_flows/calc_redirect_url.dart';
+import 'package:fluffychat/widgets/adaptive_dialogs/show_modal_action_popup.dart';
 
 Future<void> ssoLoginFlow(
   Client client,
   BuildContext context,
   bool signUp,
+  List<LoginFlow> loginFlows,
 ) async {
   final (redirectUrl, urlScheme) = calcRedirectUrl(withAuthHtmlPath: true);
 
   Logs().i('Starting legacy SSO Flow with redirect URL', redirectUrl);
 
+  final ssoProviders =
+      (loginFlows
+                  .firstWhere((flow) => flow.type == 'm.login.sso')
+                  .additionalProperties['identity_providers']
+              as List?)
+          ?.map(
+            (json) => (
+              name: json['name'] as String,
+              id: json['id'] as String,
+              brand: json['brand'] as String?,
+              icon: json['icon'] as String?,
+            ),
+          )
+          .toList();
+
+  final provider = ssoProviders == null
+      ? null
+      : await showModalActionPopup(
+          context: context,
+          title: L10n.of(context).logInTo(client.homeserver!.host),
+          actions: ssoProviders
+              .map(
+                (provider) =>
+                    AdaptiveModalAction(label: provider.name, value: provider),
+              )
+              .toList(),
+        );
+
   final url = client.homeserver!.replace(
-    path: '/_matrix/client/v3/login/sso/redirect',
+    path:
+        '/_matrix/client/v3/login/sso/redirect${provider == null ? '' : '/${provider.id}'}',
     queryParameters: {
       'redirectUrl': redirectUrl.toString(),
       'action': signUp ? 'register' : 'login',
