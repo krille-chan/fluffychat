7ca73d170ebd96349780393bd5297c8e617960f6
krille-chan <krille-chan@users.noreply.github.com>
2026-04-10 22:14:33 +0000

    deploy: e32e87e7b7de306d767842691336ab6799cec997

--- nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
+++ nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
@@ -1595,7 +1595,7 @@ let wasm_bindgen;
     };
 
     function __wbg_adapter_40(arg0, arg1, arg2) {
-        wasm.closure593_externref_shim(arg0, arg1, arg2);
+        wasm.closure586_externref_shim(arg0, arg1, arg2);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -1955,8 +1955,8 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper1739 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 594, __wbg_adapter_40);
+        imports.wbg.__wbindgen_closure_wrapper1730 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 587, __wbg_adapter_40);
             return ret;
         };
         imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "415518467" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2248330691" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
