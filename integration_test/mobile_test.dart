a75a83764a264f8d2c1c9b7bca74cd44b7ba9b76
ION606 <opensource@ion606.com>
2026-04-11 08:37:46 -0700

    remove OS lock and added testing for combinations (includes sendKeyCombo test func)

--- integration_test/mobile_test.dart
+++ integration_test/mobile_test.dart
@@ -4,6 +4,7 @@ import 'package:integration_test/integration_test.dart';
 import 'flows/auth_flows.dart';
 import 'flows/basic_messaging.dart';
 import 'flows/chat_flows.dart';
+import 'flows/keyboard_shortcuts.dart';
 import 'flows/login_and_chat_backup.dart';
 
 void main() {
@@ -12,6 +13,7 @@ void main() {
   group('FluffyChat Integration Tests', () {
     testWidgets('Login and logout flow', loginAndChatBackup);
     testWidgets('Basic Messaging', basicMessaging);
+    testWidgets('Keyboard shortcuts', keyboardShortcuts);
     testWidgets('Archive chats', archiveChats);
     testWidgets('Final logout', finalLogout);
   });
