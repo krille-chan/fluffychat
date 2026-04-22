3d2b4a80fbf89ed39e14b4e320c0ed06173a87d6
krille-chan <krille-chan@users.noreply.github.com>
2026-04-22 09:30:43 +0000

    deploy: ae454b3d3dfd87255d02585242b0b30823216fc1

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48ab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2015667038" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2526118465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
