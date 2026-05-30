49299b252f0a916115798b3b2f455488f6c725d4
krille-chan <krille-chan@users.noreply.github.com>
2026-05-30 05:56:42 +0000

    deploy: 49f5c340a77ba369a4bf821399721b204a4a4c6c

--- nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
+++ nightly/assets/assets/vodozemac/vodozemac_bindings_dart.js
@@ -1595,7 +1595,7 @@ let wasm_bindgen;
     };
 
     function __wbg_adapter_40(arg0, arg1, arg2) {
-        wasm.closure590_externref_shim(arg0, arg1, arg2);
+        wasm.closure589_externref_shim(arg0, arg1, arg2);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -1955,8 +1955,8 @@ let wasm_bindgen;
             const ret = false;
             return ret;
         };
-        imports.wbg.__wbindgen_closure_wrapper1747 = function(arg0, arg1, arg2) {
-            const ret = makeMutClosure(arg0, arg1, 591, __wbg_adapter_40);
+        imports.wbg.__wbindgen_closure_wrapper1744 = function(arg0, arg1, arg2) {
+            const ret = makeMutClosure(arg0, arg1, 590, __wbg_adapter_40);
             return ret;
         };
         imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
