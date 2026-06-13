53a13a219bc87b7bdcc5da540c98847da6d0a53d
Christian Kußowski <chritian-kussowski@posteo.de>
2026-06-13 12:45:58 +0200

    feat: Switch between display navrail mode with menu button

--- lib/pages/settings_style/settings_style_view.dart
+++ lib/pages/settings_style/settings_style_view.dart
@@ -20,7 +20,6 @@ import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
 
 import '../../config/app_config.dart';
-import '../../widgets/settings_switch_list_tile.dart';
 import 'settings_style.dart';
 
 class SettingsStyleView extends StatelessWidget {
@@ -351,10 +350,6 @@ class SettingsStyleView extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            SettingsSwitchListTile.adaptive(
-              title: L10n.of(context).displayNavigationRail,
-              setting: AppSettings.displayNavigationRail,
             ),
           ],
         ),
