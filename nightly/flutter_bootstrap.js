8f695a7ca02c20250e23b704a2d4ea69598d252d
krille-chan <krille-chan@users.noreply.github.com>
2026-06-13 11:08:30 +0000

    deploy: 412822a9b75fccdc5ac3dc943b635c72fb262767

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2437116795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3415297702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
