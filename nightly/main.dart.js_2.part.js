3021e1e715522f66c146609d346d1ec2c309c4d1
krille-chan <krille-chan@users.noreply.github.com>
2026-08-17 13:46:15 +0000

    deploy: 61457eb8a230fe784a84ea8125e975b4678e9fbd

--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -1,10 +1,10 @@
 ((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aL:function aL(){}}
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aM:function aM(){}}
 B=c[0]
 A=a.updateHolder(c[58],A)
-A.aL.prototype={
-F(d,e){return this.gbu(this).$1(e)}}
+A.aM.prototype={
+F(d,e){return this.gbw(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.aL,B.C)})()};
-(a=>{a["4Six+obwF8PoNbLDqqPuuxdjaeo="]=a.current})($__dart_deferred_initializers__);
+y(A.aM,B.C)})()};
+(a=>{a["Sf4mwu0Xjja2HJN+Z6i55VNn8IM="]=a.current})($__dart_deferred_initializers__);
 //# sourceMappingURL=main.dart.js_2.part.js.map
