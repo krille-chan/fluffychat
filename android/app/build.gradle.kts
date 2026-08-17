4d6b26fa73cebb76f7d7ef75770bd120b17920ef
Christian Kußowski <chritian-kussowski@posteo.de>
2026-08-17 10:22:33 +0200

    build: update gradle and kotlin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -35,17 +34,14 @@ configurations.all {
 
 android {
     namespace = "chat.fluffy.fluffychat"
-    compileSdk = flutter.compileSdkVersion
+    // https://github.com/juliansteenbakker/flutter_secure_storage/issues/1224
+    compileSdk = 37//flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true // Required by flutter_local_notifications
     }
 
     signingConfigs {
@@ -85,8 +81,15 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+    }
+}
+
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
