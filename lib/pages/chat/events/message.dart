e9d8170a3cb46a68067ec45b9094a8314862e5a3
Christian Kußowski <chritian-kussowski@posteo.de>
2026-08-17 08:46:03 +0200

    fix: remove transparent displayname in dm rooms

--- lib/pages/chat/events/message.dart
+++ lib/pages/chat/events/message.dart
@@ -344,7 +344,9 @@ class Message extends StatelessWidget {
                                             .onPrimaryContainer,
                                       ),
                                     ),
-                                  if ((!nextEventSameSender) && !ownMessage)
+                                  if ((!nextEventSameSender) &&
+                                      !ownMessage &&
+                                      !event.room.isDirectChat)
                                     FutureBuilder<User?>(
                                       future: event.fetchSenderUser(),
                                       builder: (context, snapshot) {
@@ -359,16 +361,15 @@ class Message extends StatelessWidget {
                                             displayname,
                                             style: TextStyle(
                                               fontWeight: FontWeight.bold,
-                                              color: event.room.isDirectChat
-                                                  ? Colors.transparent
-                                                  : (theme.brightness ==
-                                                            Brightness.light
-                                                        ? displayname
-                                                              .colorScheme
-                                                              .primary
-                                                        : displayname
-                                                              .colorScheme
-                                                              .primaryContainer),
+                                              color:
+                                                  (theme.brightness ==
+                                                      Brightness.light
+                                                  ? displayname
+                                                        .colorScheme
+                                                        .primary
+                                                  : displayname
+                                                        .colorScheme
+                                                        .primaryContainer),
                                               fontSize: 11,
                                               shadows: wallpaperTextShadow,
                                             ),
