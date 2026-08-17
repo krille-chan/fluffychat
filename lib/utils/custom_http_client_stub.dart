2cdb0514bc78f19a2948eb71027c6d81e671b64a
Christian Kußowski <chritian-kussowski@posteo.de>
2026-08-17 11:38:22 +0200

    Revert "feat: use cronet with http3 on android"
    This reverts commit 9473c405478f2d2835ce8488f7156d42b2292f33.

--- lib/utils/custom_http_client_stub.dart
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2019-Present Christian Kußowski
-// SPDX-FileCopyrightText: 2019-Present Contributors to FluffyChat
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-import 'package:http/http.dart' as http;
-
-/// Default HTTP client for platforms without Cronet (e.g. web).
-http.Client createPlatformHttpClient() => http.Client();
