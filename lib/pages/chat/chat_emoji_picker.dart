544ec4a1ab2348a28de2de3e0ccc67907d61afb2
ludmila-lovelace <ada.bolshevik@gmail.com>
2026-08-29 11:03:48 +1000

    feat: use custom emoji picker widget and add search

--- lib/pages/chat/chat_emoji_picker.dart
+++ lib/pages/chat/chat_emoji_picker.dart
@@ -3,9 +3,9 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/l10n/l10n.dart';
+import 'package:fluffychat/pages/chat/emoji_picker_dialog.dart';
 import 'package:fluffychat/pages/chat/sticker_picker_dialog.dart';
 import 'package:fluffychat/pages/chat/trust_user_key_dialog.dart';
 import 'package:material_ui/material_ui.dart';
@@ -19,7 +19,6 @@ class ChatEmojiPicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return AnimatedContainer(
       duration: FluffyThemes.animationDuration,
       curve: FluffyThemes.animationCurve,
@@ -42,37 +41,9 @@ class ChatEmojiPicker extends StatelessWidget {
                   Expanded(
                     child: TabBarView(
                       children: [
-                        EmojiPicker(
+                        EmojiPickerDialog(
                           onEmojiSelected: controller.onEmojiSelected,
                           onBackspacePressed: controller.emojiPickerBackspace,
-                          config: Config(
-                            locale: Localizations.localeOf(context),
-                            emojiViewConfig: EmojiViewConfig(
-                              noRecents: const NoRecent(),
-                              backgroundColor:
-                                  theme.colorScheme.onInverseSurface,
-                            ),
-                            bottomActionBarConfig: const BottomActionBarConfig(
-                              enabled: false,
-                            ),
-                            categoryViewConfig: CategoryViewConfig(
-                              backspaceColor: theme.colorScheme.primary,
-                              iconColor: theme.colorScheme.primary.withAlpha(
-                                128,
-                              ),
-                              iconColorSelected: theme.colorScheme.primary,
-                              indicatorColor: theme.colorScheme.primary,
-                              backgroundColor: theme.colorScheme.surface,
-                            ),
-                            skinToneConfig: SkinToneConfig(
-                              dialogBackgroundColor: Color.lerp(
-                                theme.colorScheme.surface,
-                                theme.colorScheme.primaryContainer,
-                                0.75,
-                              )!,
-                              indicatorColor: theme.colorScheme.onSurface,
-                            ),
-                          ),
                         ),
                         StickerPickerDialog(
                           room: controller.room,
@@ -102,24 +73,6 @@ class ChatEmojiPicker extends StatelessWidget {
               ),
             )
           : null,
-    );
-  }
-}
-
-class NoRecent extends StatelessWidget {
-  const NoRecent({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(
-          L10n.of(context).emoteKeyboardNoRecents,
-          style: Theme.of(context).textTheme.bodyLarge,
-          textAlign: TextAlign.center,
-        ),
-      ),
     );
   }
 }
