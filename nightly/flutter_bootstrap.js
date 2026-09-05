cf69fa339ed592006649123ada1c19e680478752
krille-chan <krille-chan@users.noreply.github.com>
2026-09-05 15:54:03 +0000

    deploy: e7457c25027423fec14f65b6fddc2e4c64a03a33

--- nightly/flutter_bootstrap.js
+++ nightly/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "358543077" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3766658023" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- nightly/main.dart.js_2.part.js
+++ nightly/main.dart.js_2.part.js
@@ -3,8 +3,8 @@ $__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={aT:function a
 B=c[0]
 A=a.updateHolder(c[59],A)
 A.aT.prototype={
-C(d,e){return this.gbw(this).$1(e)}}
+C(d,e){return this.gbv(this).$1(e)}}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
 y(A.aT,B.D)})()};
-(a=>{a["YKMEgxmReSzZG0HXbAB3+O1BYRQ="]=a.current})($__dart_deferred_initializers__);
+(a=>{a["NWpuT7CnRmC+lN8BfhlUSYQRn3k="]=a.current})($__dart_deferred_initializers__);
 //# sourceMappingURL=main.dart.js_2.part.js.map
