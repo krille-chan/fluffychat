f5b71bd43ceb38155d7d0615372079f9782dc461
krille-chan <krille-chan@users.noreply.github.com>
2026-09-19 06:08:13 +0000

    deploy: 2bf37e2915829b27ba3884627fd7ffea6c9ea8a0

--- nightly/assets/assets/vodozemac/0.8.1/vodozemac_bindings_dart.js
+++ nightly/assets/assets/vodozemac/0.8.1/vodozemac_bindings_dart.js
@@ -1595,7 +1595,7 @@ let wasm_bindgen;
     }
 
     function __wbg_adapter_43(arg0, arg1) {
-        wasm._dyn_core_5a1c124ab6a0d558___ops__function__FnMut_____Output______as_wasm_bindgen_b0ae6e9285410b9e___closure__WasmClosure___describe__invoke______(arg0, arg1);
+        wasm._dyn_core_e0ff5792a60ab54c___ops__function__FnMut_____Output______as_wasm_bindgen_8c3f0304d8828619___closure__WasmClosure___describe__invoke______(arg0, arg1);
     }
 
     const WorkerPoolFinalization = (typeof FinalizationRegistry === 'undefined')
